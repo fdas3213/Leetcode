7eae7cb3fd6bca2aade5fbfee45073dee2141e1b
Shuzhe Xiao <shuzhe@umich.edu>
2017-06-11 12:35:05 -0700

    Update 7. Reverse Integer.py

--- 7. Reverse Integer.py
+++ 7. Reverse Integer.py
@@ -1,13 +1,12 @@
 #using recursion
-def rev(num):
-	if -10 < num < 10:
-		return num
-	elif num % 10 == 0:
-		return 0
-	elif num < -10 and num %10 != 0:
-		str_num = str(num)[1:]
-		sign = str(num)[0]
-		return int(sign + str_num[-1] + str(rev(int(str_num[:-1]))))
-	else:
-		str_num = str(num)
-		return int(str_num[-1] + str(rev(int(str_num[:-1]))))
+def reverse(num):
+	if num < 0:
+		return -reverse(-num)
+
+	result = 0
+	while num != 0:
+		result = result * 10 + num %10
+		num = num // 10
+	return result
+
+print(reverse(-123))
