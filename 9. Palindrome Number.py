aeda23151b56e614afd8e38859c4a7d465a41340
Shuzhe Xiao <shuzhe@umich.edu>
2017-06-15 12:00:23 -0700

    Create 9. Palindrome Number.py

--- 9. Palindrome Number.py
+++ 9. Palindrome Number.py
@@ -1,2 +1,10 @@
 def ispalindrome(x):
-  return str(x) == str(x)[::-1]
+  if (x<0) or (x!=0 and x%10 == 0):
+    return False
+  else:
+    result = 0
+    while x != 0:
+      result = result * 10 + x%10
+      x = x//10
+    return x == result
+      
