0813e5f8b314c5fc3f43b233dc61e2e909a12630
Shuzhe Xiao <shuzhe@umich.edu>
2017-06-16 11:36:25 -0700

    Update 1. twoSum.py

--- 1. twoSum.py
+++ 1. twoSum.py
@@ -1,1 +1,15 @@
+#Temporarily a O(n^2) solution
+def twoSum(num, target):
+	index = 0
+	found = False
+	while index < len(num) and not found:
+		index2 = index + 1
 
+		while index2 < len(num) and not found:
+			result = num[index] + num[index2]
+			if result == target:
+				found = False
+				lst = [index,index2]
+			index2 += 1
+		index += 1
+	return lst
