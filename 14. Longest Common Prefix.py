004f719511c300ae254b4aa222203a06a52e16d4
Shuzhe Xiao <shuzhe@umich.edu>
2017-06-16 14:22:02 -0700

    Update 14. Longest Common Prefix.py

--- 14. Longest Common Prefix.py
+++ 14. Longest Common Prefix.py
@@ -1,1 +1,12 @@
+def longestCommonPrefix(strs):
+  i = 0 
+  stop = False
+  s = ""
+  
+  while not stop and (i < len(strs[0]) or i < len(strs[1])):
+    if strs[0][i] == strs[1][i]:
+      s += strs[0][i]
+    else:
+      stop = True
 
+  return s
