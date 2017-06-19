ceb8e93d409b6c138fd6be3c62928efed4f44eb3
Shuzhe Xiao <shuzhe@umich.edu>
2017-06-18 17:04:04 -0700

    Update 14. Longest Common Prefix.py

--- 14. Longest Common Prefix.py
+++ 14. Longest Common Prefix.py
@@ -3,16 +3,8 @@ def longestCommonPrefix(strs):
 	prefix = strs[0]
 
 	for val in strs:
-		#do not use startswith
-		#while not val.startswith(prefix):
-		#	prefix = prefix[:-1]
-		while val != prefix:
-			if len(val) > len(prefix):
-				val = val[:len(prefix) -1]
-				prefix = prefix[:-1]
-			else:
-				prefix = prefix[:len(val)-1]
-				val = val[:-1]
+		while not val.startswith(prefix):
+			prefix = prefix[:-1]
 
 	return prefix
 	
