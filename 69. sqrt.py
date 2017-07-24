0df9307118e6b3d8f8224fcf36a31bab4db316f5
Shuzhe Xiao <shuzhe@umich.edu>
2017-07-23 22:06:51 -0700

    Update 69. sqrt.py

--- 69. sqrt.py
+++ 69. sqrt.py
@@ -1,13 +1,9 @@
     def mySqrt(self, x):
         if x == 0:
-            return 0
+        return 0
+
+        guess = x
+        while guess * guess > x:
+            guess = 1/2 * (guess + x / guess)
         
-        guess = 1
-        quotient = x / guess
-        
-        while abs(guess * guess - x) > 0.001:
-            mean = (guess + quotient) / 2
-            guess = mean
-            quotient = x / guess
-            
-        return int(guess)
+        return guess
