226dc02edd6aa957100b2c780e71dfbfd2e29a37
Shuzhe Xiao <shuzhe@umich.edu>
2017-07-24 16:57:36 -0700

    Update 3. longestsubstring.py

--- 3. longestsubstring.py
+++ 3. longestsubstring.py
@@ -1,24 +1,17 @@
 def lengthOfLongestSubstring(self, s):
-        index = 0
-        s1,s2 = "", ""
-        while index < len(s):
-            if s[index] not in s1:
-                s1 += s[index]
-            else:
-                if len(s1) >= len(s2):
-                    s2,s1 = s1, ""
-                    ind = s2.index(s[index]) + 1
+    index = 0
+    s1,s2 = "", ""
+    while index < len(s):
+        if s[index] not in s1:
+            s1 += s[index]
+        else:
+            if len(s1) >= len(s2):
+                s2 = s1
+            s1 = s1[s1.index(s[index]) + 1:]
+            s1 += s[index]
 
-                    while ind < len(s2):
-                        s1 += s2[ind]
-                        ind += 1
-                    
-                else:
-                    s1 = s1[s1.index(s[index]) + 1:]
-                s1 += s[index]
+        index += 1
 
-            index += 1
-
-        result = len(s1) if len(s1) > len(s2) else len(s2)
+    result = s1 if len(s1) > len(s2) else s2
     
-        return result
+    return result
