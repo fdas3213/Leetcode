49c5ec6f6ff84c79559108ddc89a7f39272f765e
fdas3213 <szx9404@gmail.com>
2018-04-13 15:57:51 -0700

    new problems

--- ListNode.java
+++ ListNode.java
@@ -1,8 +1,14 @@
+import java.util.List;
+
 public class ListNode {
     int val;
     ListNode next;
 
     ListNode(int x){
         this.val = x;
+    }
+    ListNode(int x, ListNode n){
+        this.val = x;
+        this.next = n;
     }
 }
