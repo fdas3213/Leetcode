078432770472b789ffb2ab7b186f9676a2c1da17
Shuzhe Xiao <shuzhe@umich.edu>
2018-04-22 16:15:14 -0700

    Update 13. RomanToInteger.py

--- 13. RomanToInteger.py
+++ 13. RomanToInteger.py
@@ -1,13 +1,10 @@
 def romanToInt(self, s):
-        roman_dict = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100,'D':500,'M':1000}
-        result = roman_dict[s[0]]
-        index = 1
-        
-        while index < len(s):
-            if roman_dict[s[index]] > roman_dict[s[index - 1]]:
-                result -= 2 * roman_dict[s[index - 1]]
-                result += roman_dict[s[index]]
+        d = {"I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000}
+        out = 0
+        for i in range(len(s) - 1):
+            if d[s[i]] < d[s[i+1]]:
+                out -= d[s[i]]
             else:
-                result += roman_dict[s[index]]
-            index += 1
-        return result
+                out += d[s[i]]
+        out += d[s[-1]]
+        return out
