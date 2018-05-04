eda4b21eecab386b040ba6fab17ab23b19bfbc6b
fdas3213 <szx9404@gmail.com>
2018-05-04 13:21:29 -0700

    added 67

--- addbinary67.java
+++ addbinary67.java
@@ -2,7 +2,7 @@ import java.lang.Math;
 
 public class addbinary67 {
     public static void main(String[] args){
-        //System.out.println(addBinary("10","11"));
+        System.out.println(addBinary("10","11"));
         System.out.println(addBinary("110","10"));
     }
 
@@ -12,21 +12,20 @@ public class addbinary67 {
         int i = a.length() - 1;
         int j = b.length() - 1;
         String out = "";
+        int reminder = 0;
 
-        while (i >= 0 && j >= 0){
+        while (i >= 0 || j >= 0){
             int sum = Character.getNumericValue(a.charAt(i)) + Character.getNumericValue(b.charAt(j));
-            if (sum == 2){
+            if (sum + reminder == 2){
                 out += "0";
+                reminder = 1;
+            }else if (sum + reminder > 2){
                 out += "1";
-            }else out += Integer.toString(sum);
+                reminder = 1;
+            }else out += sum;
             i --;
             j --;
-
         }
-
-        if (a.length() > b.length()){
-            out += a.substring(0,i);
-        }else if (b.length() > a.length()) out += b.substring(0,j);
 
         return out;
     }
