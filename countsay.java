0b62218dba5206cfac22796c7dba9fc48ffaf93d
Shuzhe Xiao <parker@Shuzhes-MacBook-Pro.local>
2018-05-11 14:10:15 -0700

    workspce

---
+++ countsay.java
@@ -0,0 +1,10 @@
+public class countsay {
+    public static void main(String[] args){}
+
+    public static String countAndSay(int n){
+        if (n == 1) return "1";
+        if (n == 2) return "11";
+
+
+    }
+}
