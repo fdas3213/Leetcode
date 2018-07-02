5ddb3fe092c6ed471f2e61f9d310512fe2da4614
fdas3213 <szx9404@gmail.com>
2018-07-02 15:34:41 -0700

    add treenode

--- TreeNode.java
+++ TreeNode.java
@@ -3,4 +3,10 @@ public class TreeNode {
     TreeNode left;
     TreeNode right;
     TreeNode(int x) { val = x; }
+
+    public static void toString(TreeNode n){
+        System.out.println(" " + n.val + " ");
+        if (n.left != null) toString(n.left);
+        if (n.right != null) toString(n.right);
+    }
 }
