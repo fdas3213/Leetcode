82c77fc3e1bd040ec8689a77ec5a9a97c85b2caa
fdas3213 <szx9404@gmail.com>
2018-09-05 17:52:08 -0700

    finished 26

--- LC26.java
+++ LC26.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
-
 public class LC26 {
+    public static int removeDuplicates(int[] nums){
+        int start = 0, i = 0;
+        while (i < nums.length) {
+            nums[start] = nums[i];
+            do {
+                i++;
+            } while (i < nums.length && nums[i] == nums[start]);
+            start++;
+        }
+        return start;
+    }
+
+    public static void main(String[] args){
+        int[] t1 = {1,1,2};
+        int[] t2= {0,0,1,1,1,2,2,3,3,4};
+        System.out.println(removeDuplicates(t1));
+        System.out.println(removeDuplicates(t2));
+    }
 }
