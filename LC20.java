f20b940fe63ca65e44744929b475865df9496b7f
fdas3213 <szx9404@gmail.com>
2018-09-05 17:53:02 -0700

    finished 20

--- LC20.java
+++ LC20.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
+import java.util.Stack;
 
 public class LC20 {
+    public static boolean isValid(String s) {
+        Stack<Character> st = new Stack<Character>();
+
+        for (char c: s.toCharArray()){
+            if (c == '(') st.push(')');
+            else if (c == '[') st.push(']');
+            else if (c == '{') st.push('}');
+            else if (st.isEmpty() || st.pop() != c) return false;
+        }
+        return st.isEmpty();
+    }
+
+    public static void main(String[] args){
+        String t1 = "()[]{}";
+        System.out.println(isValid(t1));
+    }
 }
