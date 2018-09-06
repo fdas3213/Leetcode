17f7330fe39ed1e195cac2cf4eb1ea28d024b3a3
fdas3213 <szx9404@gmail.com>
2018-09-05 18:54:18 -0700

    added 35 with binarysearch

--- LC35.java
+++ LC35.java
@@ -7,6 +7,17 @@ public class LC35 {
         return nums.length;
     }
 
+    public static int binarysearchInsert(int[] nums, int target){
+        int low = 0, high = nums.length-1;
+        while(low <= high){
+            int mid = low + (high-low)/2;
+            if(nums[mid] == target) return mid;
+            else if(nums[mid] > target) high = mid -1;
+            else low = mid+1;
+        }
+        return low;
+    }
+
     public static void main(String[] args){
         int[] arr1 = {1,3,5,6};
         System.out.println(searchInsert(arr1,6));
