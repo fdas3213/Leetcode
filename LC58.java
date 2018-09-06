2c13cde8c23fe60e8a0581e49381d758f5ea6d07
fdas3213 <szx9404@gmail.com>
2018-09-06 13:40:35 -0700

    added 2 solutions of 58

--- LC58.java
+++ LC58.java
@@ -1,13 +1,33 @@
 public class LC58 {
 
-    public static int lengthOfLastWord(String s){
+    public static int lengthOfLastWord(String s) {
+        if (s.length() == 0 || s == " ") return 0;
+        int l = s.length();
+        int k = 0;
+
+        for (int i = l - 1; i >= 0; i --){
+            if (s.charAt(i) == ' ' && k > 0) return k;
+            else if(s.charAt(i) != ' ') k++;
+        }
+        return k;
+    }
+
+    public static int lengthOfLastWordSolution2(String s){
         int l = s.length();
         if (l == 0 || s.equals(" ")) return 0;
-        int index = -1;
-        for (int i = 0; i < l;i++){
-            if(s.charAt(i) == ' ') index = i;
+        int lastwordl = 0;
+        int i = 0;
+        while(i < l){
+            int out = 0;
+            if(s.charAt(i) != ' '){
+                for (int j = i; j< l && s.charAt(j) != ' '; j++){
+                    out++;
+                }
+                lastwordl = out;
+            }else out++;
+            i += out;
         }
-        return s.length() - index -1;
+        return lastwordl;
     }
 
     public static void main(String[] args){
@@ -15,6 +35,10 @@ public class LC58 {
         System.out.println(lengthOfLastWord(s1));
         String s2 = "a";
         System.out.println(lengthOfLastWord(s2));
+        String s3 = " a";
+        System.out.println(lengthOfLastWord(s3));
+        String s4 = "a ";
+        System.out.println(lengthOfLastWord(s3));
     }
 
 }
