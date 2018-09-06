3fcb09c0b7bb476211e960bd02e4f8ff7b84ff4e
fdas3213 <szx9404@gmail.com>
2018-09-05 22:55:03 -0700

    need to be modified

--- LC58.java
+++ LC58.java
@@ -1,22 +1,20 @@
 public class LC58 {
-        /*
-        Given a string s consists of upper/lower-case alphabets and empty space characters ' ', return the length of last word in the string.
 
-    If the last word does not exist, return 0.
-
-    Note: A word is defined as a character sequence consists of non-space characters only.
-
-    Example:
-
-    Input: "Hello World"
-    Output: 5
-         */
-        public static int lengthOfLastWord(String s){
-
+    public static int lengthOfLastWord(String s){
+        int l = s.length();
+        if (l == 0 || s.equals(" ")) return 0;
+        int index = -1;
+        for (int i = 0; i < l;i++){
+            if(s.charAt(i) == ' ') index = i;
         }
+        return s.length() - index -1;
+    }
 
-        public static void main(String[] args){
-
-        }
+    public static void main(String[] args){
+        String s1 = "Hello World";
+        System.out.println(lengthOfLastWord(s1));
+        String s2 = "a";
+        System.out.println(lengthOfLastWord(s2));
+    }
 
 }
--- LC66.java
+++ LC66.java
@@ -1,6 +1,7 @@
-public class LC66 {
-    /*
-    Given a non-empty array of digits representing a non-negative integer, plus one to the integer.
+import java.util.Arrays;
+
+/*
+Given a non-empty array of digits representing a non-negative integer, plus one to the integer.
 
 The digits are stored such that the most significant digit is at the head of the list, and each element in the array contain a single digit.
 
@@ -16,10 +17,33 @@ Example 2:
 Input: [4,3,2,1]
 Output: [4,3,2,2]
 Explanation: The array represents the integer 4321.
-     */
-    public static int[] plutOne(int[] digits){}
+
+*/
+public class LC66 {
+    public static int[] plutOne(int[] digits){
+        int num = 0;
+        int l = 0;
+        for (int i = 0; i < digits.length;i++){
+            num = num * 10 + digits[i];
+            l++;
+        }
+        num ++;
+
+        int[] out = new int[l];
+        for (int k = l-1; k >= 0; k--){
+            out[k] = num % 10;
+            num /= 10;
+        }
+        return out;
+    }
 
     public static void main(String[] args){
-
+        int[] t1= {1,2,3};
+        System.out.println(Arrays.toString(plutOne(t1)));
+        int[] t2= {4,3,2,1};
+        System.out.println(Arrays.toString(plutOne(t2)));
+        int[] t3 = {8,9,9,9};
+        System.out.println(Arrays.toString(plutOne(t3)));
     }
 }
+
