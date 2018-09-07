0e65fdddf7d0efbbbcb245044bf4078f6e2bcf36
fdas3213 <szx9404@gmail.com>
2018-09-07 15:27:39 -0700

    added LC104, unfinished yet

---
+++ LC104.java
@@ -0,0 +1,18 @@
+
+public class LC104 {
+    public static int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        
+    }
+
+    public static void main(String[] args){
+        TreeNode t = new TreeNode(3);
+        t.left = new TreeNode(9);
+        t.right = new TreeNode(20);
+        t.right.left = new TreeNode(15);
+        t.right.right = new TreeNode(7);
+
+        System.out.println(maxDepth(t));
+        TreeNode.toString(t);
+    }
+}
