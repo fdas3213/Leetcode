63a73a6262500a9db7e5cfadd07be3304c464cd9
fdas3213 <szx9404@gmail.com>
2018-09-07 15:49:35 -0700

    edits toString()

--- TreeNode.java
+++ TreeNode.java
@@ -9,4 +9,21 @@ public class TreeNode {
         if (n.left != null) toString(n.left);
         if (n.right != null) toString(n.right);
     }
+
+    public static String traverse(TreeNode n){
+        StringBuilder s = new StringBuilder();
+        s.append(n.val);
+        System.out.println(n.val);
+        if (n.left != null) traverse(n.left);
+        if (n.right != null) traverse(n.right);
+        return s.toString();
+    }
+
+    public static void main(String[] args){
+        TreeNode t = new TreeNode(5);
+        t.left = new TreeNode(7);
+        t.right = new TreeNode(10);
+        //TreeNode.toString(t);
+        System.out.println(traverse(t));
+    }
 }
