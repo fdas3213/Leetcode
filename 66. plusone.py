d1469a4e296fd447fa0f99004b92ec8d11be7d43
fdas3213 <szx9404@gmail.com>
2018-09-06 17:05:24 -0700

    removed 66 in python

--- 66. plusone.py
+++
@@ -1,8 +0,0 @@
-def plusOne(self, digits):
-        index = 0
-        result = 0
-        while index < len(digits):
-            result = result * 10 + digits[index]
-            index += 1
-        
-        return [int(i) for i in str(result+1)]  
