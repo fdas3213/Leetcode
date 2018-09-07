c43c38b732594879a4bc1457e5160f78fa41bf1f
fdas3213 <szx9404@gmail.com>
2018-09-07 12:25:28 -0700

    added 83

--- LC83.java
+++ LC83.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
-
 public class LC83 {
+    public static ListNode deleteDuplicates(ListNode head) {
+        if (head == null) return head;
+        ListNode out = new ListNode(0);
+        out.next = head;
+
+        while(head.next != null){
+            if (head.val == head.next.val) head.next = head.next.next;
+            else head = head.next;
+        }
+        return out.next;
+    }
+    public static void main(String[] args){
+        ListNode t1 = new ListNode(1,new ListNode(2,new ListNode(2,new ListNode(3,new ListNode(3)))));
+        System.out.println(ListNode.toString(deleteDuplicates(t1)));
+        ListNode t2 = new ListNode(1,new ListNode(1));
+        System.out.println(ListNode.toString(deleteDuplicates(t2)));
+        ListNode t3 = new ListNode(1,new ListNode(1, new ListNode(1)));
+        System.out.println(ListNode.toString(deleteDuplicates(t3)));
+    }
 }
