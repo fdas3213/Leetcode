d01feb31d83ce75d574643115331c9ccbb0ac083
fdas3213 <szx9404@gmail.com>
2018-09-09 16:10:47 -0700

    modified

--- TreeNode.java
+++ TreeNode.java
@@ -10,20 +10,37 @@ public class TreeNode {
         if (n.right != null) toString(n.right);
     }
 
-    public static String traverse(TreeNode n){
-        StringBuilder s = new StringBuilder();
-        s.append(n.val);
+    public static void inorder(TreeNode n){
+        if (n.left != null) inorder(n.left);
         System.out.println(n.val);
-        if (n.left != null) traverse(n.left);
-        if (n.right != null) traverse(n.right);
-        return s.toString();
+        if (n.right != null) inorder(n.right);
     }
 
+    public static void preorder(TreeNode n){
+        System.out.println(n.val);
+        if (n.left != null) preorder(n.left);
+        if (n.right != null) preorder(n.right);
+    }
+
+    public static void postorder(TreeNode n){
+        if (n.left != null) postorder(n.left);
+        if (n.right != null) postorder(n.right);
+        System.out.println(n.val);
+    }
+
+
     public static void main(String[] args){
-        TreeNode t = new TreeNode(5);
-        t.left = new TreeNode(7);
-        t.right = new TreeNode(10);
+        TreeNode t = new TreeNode(3);
+        t.left = new TreeNode(9);
+        t.right = new TreeNode(20);
+        t.right.left = new TreeNode(15);
+        t.right.right = new TreeNode(7);
         //TreeNode.toString(t);
-        System.out.println(traverse(t));
+        System.out.println("Inorder");
+        TreeNode.inorder(t);
+        System.out.println("Preorder");
+        TreeNode.preorder(t);
+        System.out.println("Postorder");
+        TreeNode.postorder(t);
     }
 }
