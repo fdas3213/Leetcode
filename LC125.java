26f17edb53e4f9c2abae401356c419ac7a06add3
fdas3213 <szx9404@gmail.com>
2018-09-09 22:00:33 -0700

    finished palindrome string

---
+++ LC125.java
@@ -0,0 +1,27 @@
+public class LC125 {
+
+    public static boolean isPalindrome(String s){
+        if (s == "" || s == null) return false;
+        int start = 0, end = s.length()-1;
+        s = s.toLowerCase();
+        char char_s, char_e;
+        while (start <= end){
+            char_s = s.charAt(start);
+            char_e = s.charAt(end);
+            if (!Character.isLetterOrDigit(char_s)) start++;
+            else if(!Character.isLetterOrDigit(char_e)) end--;
+            else {
+                if (char_s != char_e) return false;
+                start++;
+                end--;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args){
+        String t1 = "A man, a plan, a canal: Panama";
+        System.out.println(isPalindrome(t1));
+    }
+
+}
