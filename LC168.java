d9af3f6fbdf2148cb2582a182fbec9bb89f85417
fdas3213 <szx9404@gmail.com>
2018-09-10 18:50:36 -0700

    added P168

--- LC168.java
+++ LC168.java
@@ -1,29 +1,4 @@
-/*
-Given a positive integer, return its corresponding column title as appear in an Excel sheet.
 
-For example:
-
-    1 -> A
-    2 -> B
-    3 -> C
-    ...
-    26 -> Z
-    27 -> AA
-    28 -> AB
-    ...
-Example 1:
-
-Input: 1
-Output: "A"
-Example 2:
-
-Input: 28
-Output: "AB"
-Example 3:
-
-Input: 701
-Output: "ZY"
- */
 public class LC168 {
     public static String convertToTitle(int n) {
         if (n == 0) return null;
@@ -34,8 +9,9 @@ public class LC168 {
             if (n%26 == 0) div = (n-1)/26 -1;
             else div = n/26 -1;
             return Character.toString((char)((int)'A' + div)) + Character.toString((char)((int)'A'+mod));
+        }else{
+            return convertToTitle(n/26) + Character.toString((char)((int)'A' + mod)) ;
         }
-        else
     }
 
     public static void main(String[] args){
@@ -43,5 +19,6 @@ public class LC168 {
         System.out.println(convertToTitle(28));
         System.out.println(convertToTitle(702));
         System.out.println(convertToTitle(52));
+        System.out.println(convertToTitle(704));
     }
 }
