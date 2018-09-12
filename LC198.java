3356452003fe2ed9dd72ed15a0734f077e745411
fdas3213 <szx9404@gmail.com>
2018-09-11 21:14:45 -0700

    finished 198 dp

--- LC198.java
+++ LC198.java
@@ -1,41 +1,30 @@
-/*
-You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed,
-the only constraint stopping you from robbing each of them is that adjacent houses have security system connected
-and it will automatically contact the police if two adjacent houses were broken into on the same night.
-
-Given a list of non-negative integers representing the amount of money of each house,
-determine the maximum amount of money you can rob tonight without alerting the police.
-
-Example 1:
-
-Input: [1,2,3,1]
-Output: 4
-Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
-             Total amount you can rob = 1 + 3 = 4.
-Example 2:
-
-Input: [2,7,9,3,1]
-Output: 12
-Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 (money = 1).
-             Total amount you can rob = 2 + 9 + 1 = 12.
-
- */
-
 public class LC198 {
     public static int rob(int[] nums) {
-        int odd = 0, even = 0;
-        for (int i = 0; i < nums.length; i++){
-            if (i%2 == 0) even+= nums[i];
-            else odd += nums[i];
+        if (nums.length == 0) return 0;
+        int[] dp = new int[nums.length+1];
+        dp[0] = 0;
+        dp[1] = nums[0];
+        for (int i = 1; i <nums.length;i++){
+            int val = nums[i];
+            dp[i+1] = Math.max(val + dp[i-1], dp[i]);
         }
-        return Math.max(odd, even);
+        return dp[nums.length];
     }
 
     public static void main(String[] args){
+
         int[] t1 = {1,2,3,1};
         int[] t2 = {2,7,9,3,1};
+        int[] t3 = {2,1,1,2};
+
         System.out.println(rob(t1));
         System.out.println(rob(t2));
+        System.out.println(rob(t3));
+
+        int[] t4 = {1,3,1,3,100};
+        System.out.println(rob(t4));
+
+
 
     }
 }
