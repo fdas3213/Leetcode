7584188a94f28ac68dbfab6a48da04240b4eb0f6
fdas3213 <szx9404@gmail.com>
2018-09-12 14:45:17 -0700

    added 2 solutions

--- LC203.java
+++ LC203.java
@@ -16,9 +16,20 @@ public class LC203 {
         return out.next;
     }
 
+    public static ListNode solution2(ListNode head, int val){
+        while(head != null && head.val == val) head = head.next;
+        ListNode copy = head;
+        while(head !=null && head.next != null){
+            if(head.next.val == val) head.next = head.next.next;
+            else head = head.next;
+        }
+        return copy;
+    }
+
     public static void main(String[] args){
         ListNode n1 = new ListNode(1,new ListNode(2,new ListNode(6,new ListNode(3, new ListNode(4,
                 new ListNode(5, new ListNode(6)))))));
         System.out.println(ListNode.toString(removeElements(n1, 6)));
+        System.out.println(ListNode.toString(solution2(n1,6)));
     }
 }
