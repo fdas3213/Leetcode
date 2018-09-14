212a1acf814d8b6bfccbde2b98511cedde862685
fdas3213 <szx9404@gmail.com>
2018-09-14 10:59:00 -0700

    finished 24

--- LC24.java
+++ LC24.java
@@ -10,12 +10,26 @@ Your algorithm should use only constant extra space.
 You may not modify the values in the list's nodes, only nodes itself may be changed.
  */
 
-public class LC24 {
-    public ListNode swapPairs(ListNode head) {
 
+public class LC24 {
+    public static ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode out = new ListNode(0);
+        out.next = head;
+        ListNode copy = out;
+        while (copy.next != null && copy.next.next !=null){
+            ListNode first = copy.next;
+            ListNode sec = copy.next.next;
+            first.next = sec.next;
+            copy.next = sec;
+            copy.next.next = first;
+            copy = copy.next.next;
+        }
+        return out.next;
     }
 
     public static void main(String[] args){
-
+        ListNode l1 = new ListNode(1,new ListNode(2,new ListNode(3, new ListNode(4))));
+        System.out.println(ListNode.toString(swapPairs(l1)));
     }
 }
