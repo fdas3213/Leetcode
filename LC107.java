655114a8845991b9162dfdd6fca8b4f663ae09a3
fdas3213 <szx9404@gmail.com>
2018-10-12 14:22:23 -0700

    added LC107

--- LC107.java
+++ LC107.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Queue;
 
 public class LC107 {
+    public static List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> out = new ArrayList<>();
+        if(root == null) return out;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()){
+            List<Integer> inner = new ArrayList<>();
+            int size = queue.size();
+            for (int i = 0; i < size; i++){
+                TreeNode cur = queue.poll();
+                if(cur.left != null) queue.offer(cur.left);
+                if(cur.right != null) queue.offer(cur.right);
+                inner.add(cur.val);
+            }
+            out.add(0,inner);
+        }
+        return out;
+    }
 }
