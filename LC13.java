f07c7fc82660bbb138a1a8f03e00f693fca9a424
fdas3213 <szx9404@gmail.com>
2018-11-19 16:14:22 -0800

    has a better one

--- LC13.java
+++ LC13.java
@@ -24,6 +24,25 @@ public class LC13 {
         return sum;
     }
 
+    public static int betterone(String s){
+        HashMap<Character, Integer> hm = new HashMap<>();
+        hm.put('I',1);
+        hm.put('V',5);
+        hm.put('X',10);
+        hm.put('L',50);
+        hm.put('C', 100);
+        hm.put('D', 500);
+        hm.put('M', 1000);
+        int out = hm.get(s.charAt(0));
+        for (int i = 1; i < s.length(); i++){
+            char prev = s.charAt(i-1);
+            char cur = s.charAt(i);
+            if (hm.get(prev) < hm.get(cur)) out -= 2*hm.get(prev);
+            out += hm.get(cur);
+        }
+        return out;
+    }
+
     public static void main(String[] args){
         String s1 = "IV";
         System.out.println(romanToInt(s1));
