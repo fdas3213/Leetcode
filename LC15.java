2b4e9a5c9af5b5a530cdd531b29b9ef12b528c16
fdas3213 <szx9404@gmail.com>
2018-11-19 18:17:12 -0800

    added a faster solution

--- LC15.java
+++ LC15.java
@@ -26,6 +26,30 @@ public class LC15 {
         return listSolution;
     }
 
+    public static List<List<Integer>> faster(int[] nums){
+        //avoids duplicate by start++, end-- in else condition
+        List<List<Integer>> l = new ArrayList<>();
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length-2; i++){
+            if (i == 0 || nums[i] != nums[i-1]){
+                int start = i+1, end = nums.length - 1;
+                while (start < end){
+                    int sum = nums[i]+nums[start]+nums[end];
+                    if (sum < 0) start++;
+                    else if (sum > 0) end--;
+                    else {
+                        l.add(Arrays.asList(nums[i], nums[start], nums[end]));
+                        while(start < end && nums[start] == nums[start+1]) start++;
+                        while(start < end && nums[end] == nums[end-1]) end--;
+                        start++;
+                        end--;
+                    }
+                }
+            }
+        }
+        return l;
+    }
+
     public static void main(String[] args){
         int[] t1 = {-1, 0, 1, 2, -1, -4};
         System.out.println(threeSum(t1));
