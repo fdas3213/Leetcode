c016e41429a6fd1a377296b21119ff8de3b4e320
fdas3213 <szx9404@gmail.com>
2018-11-26 16:29:36 -0800

    added 2 more

---
+++ LC36.java
@@ -0,0 +1,34 @@
+import java.util.HashSet;
+
+public class LC36 {
+    private static boolean isValidSudoku(char[][] board){
+        for (int i = 0; i < board.length; i ++) {
+            HashSet<Character> row = new HashSet<>();
+            HashSet<Character> col = new HashSet<>();
+            HashSet<Character> square = new HashSet<>();
+            for (int j = 0; j < board.length; j++){
+                //check each row
+                if (board[i][j] != '.' && !row.add(board[i][j])) return false;
+                //check for each col
+                if (board[j][i] != '.' && !col.add(board[j][i])) return false;
+                //check for 3*3 square
+                int ridx = 3 * (i/3);
+                int cidx = 3 * (i%3);
+                if(board[ridx + j/3][cidx+j%3] != '.' && !square.add(board[ridx+j/3][cidx+j%3])) return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args){
+        HashSet<Integer> test = new HashSet<>();
+        for(int i = 0;i < 9;i++){
+            for (int j = 0;j<9;j++){
+                int ridx = 3 * (i/3);
+                int cidx = 3 * (i%3);
+                System.out.println("i: "+i + " j: "+j + " ridx:" + ridx +" cidx: "+cidx +
+                        " ridx + j/3: " + (ridx+j/3) + " cidx+j%3: " + (cidx+j%3));
+            }
+        }
+    }
+}
---
+++ LC49.java
@@ -0,0 +1,40 @@
+import com.sun.org.apache.xerces.internal.xs.StringList;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+public class LC49 {
+    private static List<List<String>> groupAnagrams(String[] strs) {
+        if(strs.length == 0 || strs == null) return new ArrayList<List<String>>();
+        HashMap<String, List<String>> m = new HashMap<>();
+        for (String s: strs){
+            char[] arr = s.toCharArray();
+            Arrays.sort(arr);
+            String key = String.valueOf(arr);
+            if (!m.containsKey(key)) m.put(key, new ArrayList<String>());
+            m.get(key).add(s);
+        }
+        return new ArrayList<List<String>>(m.values());
+    }
+
+    private static List<List<String>> groupAnaFast(String[] strs){
+        int[] primes = {2,3,5,7,11,13,17,19,23,29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101};
+        HashMap<Integer, List<String>> hm = new HashMap<>();
+        for (String s:strs){
+            int product = 1;
+            for (char c: s.toCharArray()){
+                product *= primes[c - 'a'];
+            }
+            if(!hm.containsKey(product)) hm.put(product, new ArrayList<String>());
+            hm.get(product).add(s);
+        }
+        return new ArrayList<List<String>>(hm.values());
+    }
+
+    public static void main(String[] args){
+        String[] test = {"eat", "tea", "tan", "ate", "nat", "bat"};
+        System.out.println(groupAnagrams(test));
+    }
+}
