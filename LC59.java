3c06caea6f8aad5215bc53809cae340524346644
fdas3213 <szx9404@gmail.com>
2018-12-05 09:51:18 -0800

    finished spiralmatrixII

---
+++ LC59.java
@@ -0,0 +1,52 @@
+import java.util.Arrays;
+
+public class LC59 {
+    public static int[][] generateMatrix(int n){
+        int[][] out = new int[n][n];
+        int rowStart = 0, colStart = 0;
+        int rowEnd = n-1, colEnd = n-1;
+        int val = 1;
+        while(rowStart <= rowEnd && colStart <= colEnd){
+            for(int i = colStart;i <= colEnd; i++){
+                //fill in from left to right
+                out[rowStart][i] = val++;
+            }
+            rowStart++;
+            for(int i = rowStart; i<= rowEnd; i++){
+                //fill in from top to bottom
+                out[i][colEnd] = val++;
+            }
+            colEnd--;
+            if(rowStart <= rowEnd){
+                for (int i = colEnd; i>= colStart; i--){
+                    //fill in from right to left
+                    out[rowEnd][i] = val++;
+                }
+            }
+            rowEnd--;
+            if(colStart <= colEnd){
+                for(int i = rowEnd; i >= rowStart; i--){
+                    //fill in from bottom to top
+                    out[i][colStart] = val++;
+                }
+            }
+            colStart++;
+        }
+        return out;
+        /*
+        Input: 3
+Output:
+[
+ [ 1, 2, 3 ],
+ [ 8, 9, 4 ],
+ [ 7, 6, 5 ]
+]
+         */
+    }
+    public static void main(String[] args){
+        int[][] output = generateMatrix(3);
+        for (int i = 0; i< 3; i++){
+            System.out.println(Arrays.toString(output[i]));
+        }
+    }
+}
