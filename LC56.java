218086328761b310d4d9a0cc12e858952e3cb994
fdas3213 <szx9404@gmail.com>
2018-12-05 09:32:44 -0800

    finished 56

--- LC56.java
+++ LC56.java
@@ -1,35 +1,39 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class LC56 {
     private static List<Interval> merge(List<Interval> intervals) {
-        if(intervals.size() <= 1) return intervals;
+        if (intervals.size() <= 1) return intervals;
         List<Interval> out = new ArrayList<>();
-        for(int i = 0; i < intervals.size(); i++){
-            Interval cur = intervals.get(i);
-            for (int j = i+1; j < intervals.size(); j++){
-                Interval comp = intervals.get(j);
-                if (cur.end >= comp.start) {
-                    Interval newone = new Interval(cur.start, comp.end);
-                    out.add(newone);
-                }
+        int n = intervals.size();
+        int[] start = new int[n];
+        int[] end = new int[n];
+        for (int i = 0; i < n; i++){
+            start[i] = intervals.get(i).start;
+            end[i] = intervals.get(i).end;
+        }
+        Arrays.sort(start);
+        Arrays.sort(end);
+        for(int i = 0, j =0; i < n;i++){
+            if(i == n-1 || start[i+1]>end[i]){
+                out.add(new Interval(start[j], end[i]));
+                j = i+1;
             }
         }
-        /*
-        Example 1:
-
-Input: [[1,3],[2,6],[8,10],[15,18]]
-Output: [[1,6],[8,10],[15,18]]
-Explanation: Since intervals [1,3] and [2,6] overlaps, merge them into [1,6].
-Example 2:
-
-Input: [[1,4],[4,5]]
-Output: [[1,5]]
-Explanation: Intervals [1,4] and [4,5] are considered overlapping.
-         */
+        return out;
     }
 
     public static void main(String[] args){
-
+        Interval t1 = new Interval(1,3);
+        Interval t2 = new Interval(2,6);
+        Interval t3 = new Interval(8,10);
+        Interval t4 = new Interval(15,18);
+        List<Interval> testinterval = new ArrayList<>();
+        testinterval.add(t1);
+        testinterval.add(t2);
+        testinterval.add(t3);
+        testinterval.add(t4);
+        System.out.println(merge(testinterval));
     }
 }
