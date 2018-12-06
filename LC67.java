ca5d898ae769214d36eabda49318cd5ddb242aa0
fdas3213 <szx9404@gmail.com>
2018-12-06 14:54:55 -0800

    did addbinary one more time

--- LC67.java
+++ LC67.java
@@ -15,6 +15,21 @@ public class LC67 {
         return out.reverse().toString();
     }
 
+    private static String Sol2(String a, String b){
+        StringBuilder out = new StringBuilder();
+        int l1 = a.length()-1, l2 = b.length()-1;
+        int mod = 0, sum;
+        while(l1 >=0 || l2 >= 0){
+            if(l1 >=0 && l2 >= 0) sum = (a.charAt(l1--) - '0') + (b.charAt(l2--)-'0') + mod;
+            else if (l1 < 0 && l2 >= 0) sum = b.charAt(l2--) -'0' + mod;
+            else sum = a.charAt(l1--)-'0' + mod;
+            mod = sum /2;
+            out.append(sum%2);
+        }
+        if (mod !=0) out.append(mod);
+        return out.reverse().toString();
+    }
+
     public static void main(String[] args){
         System.out.println(addBinary("1010","1011"));
         System.out.println(addBinary("1","111"));
