b68d4d6d458f890c8f7bee0d515216745ba36858
fdas3213 <szx9404@gmail.com>
2018-12-06 23:24:48 -0800

    add sqrt

--- LC69.java
+++ LC69.java
@@ -16,11 +16,21 @@ Output: 2
 Explanation: The square root of 8 is 2.82842..., and since
              the decimal part is truncated, 2 is returned.
 
+*/
 public class LC69 {
-    public static int mySqrt(int x) {}
+    public static int mySqrt(int x) {
+        if (x <= 1) return x;
+        int left = 1, right = x;
+        while (left < right){
+            int mid = left + (right - left + 1)/2;
+            if(mid >x/mid) right = mid - 1;
+            else left = mid;
+        }
+        return left;
+    }
 
     public static void main(String[] args){
-
+        System.out.println(mySqrt(8));
+        System.out.println(mySqrt(18));
     }
 }
- */
