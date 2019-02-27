213a4c12f72cbe2c6f5d9869b5484ddb57073d54
fdas3213 <szx9404@gmail.com>
2019-02-27 21:24:38 +0800

    add LC40

---
+++ LC40.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class LC40 {
+    public static List<List<Integer>> combinationSum2(int[] candidates, int target){
+        Arrays.sort(candidates);
+        List<List<Integer>> l = new ArrayList<>();
+        backtrack(l, new ArrayList<>(), candidates, target, 0);
+        return l;
+    }
+
+    private static void backtrack(List<List<Integer>> list, List<Integer> tempList, int[] nums, int remain, int start){
+        if (remain < 0) return;
+        else if (remain == 0) list.add(new ArrayList<>(tempList));
+        else{
+            for (int i = start; i < nums.length; i++){
+                //add one condition to avoid duplicates
+                if (i > start && nums[i] == nums[i-1]) continue;
+                tempList.add(nums[i]);
+                backtrack(list, tempList, nums, remain-nums[i], i+1);
+                tempList.remove(tempList.size() - 1);
+            }
+        }
+    }
+
+    public static void main(String[] args){
+        int[] t1 = {10,1,2,7,6,1,5};
+        int[] t2 = {2,5,2,1,2};
+        System.out.println(combinationSum2(t1,8));
+        System.out.println(combinationSum2(t2,5));
+    }
+}
