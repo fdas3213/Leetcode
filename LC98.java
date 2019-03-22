db7a66c85980e0515835fce54f354f14c63b7e68
fdas3213 <szx9404@gmail.com>
2019-03-22 14:43:38 -0700

    add another solution

--- LC98.java
+++ LC98.java
@@ -18,6 +18,16 @@ public class LC98 {
         return true;
     }
 
+    public static boolean Sol2(TreeNode root){
+        return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    private static boolean isValidBST(TreeNode root, long minVal, long maxVal){
+        if (root == null) return true;
+        if (root.val >= maxVal || root.val <= minVal) return false;
+        return isValidBST(root.left, minVal, root.val) && isValidBST(root.right,root.val, maxVal);
+    }
+
     public static void main(String[] args){
         TreeNode t = new TreeNode(5);
         t.left = new TreeNode(1);
@@ -26,5 +36,6 @@ public class LC98 {
         t.right.right = new TreeNode(6);
 
         System.out.println(isValidBST(t));
+        System.out.println(Sol2(t));
     }
 }
