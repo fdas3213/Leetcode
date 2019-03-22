f1668e901a26d1c83c4911cc8ae0fc9fa200b2e2
fdas3213 <szx9404@gmail.com>
2019-03-22 14:33:27 -0700

    another inorder traversal solution

---
+++ LC98.java
@@ -0,0 +1,30 @@
+import java.util.Stack;
+
+public class LC98 {
+    public static boolean isValidBST(TreeNode root) {
+        if(root ==  null) return false;
+        Stack<TreeNode> st = new Stack<>();
+        TreeNode pre = null;
+        while(!st.empty() || root != null){
+            while(root != null) {
+                st.push(root);
+                root = root.left;
+            }
+            root = st.pop();
+            if(pre != null && root.val <= pre.val) return false;
+            pre = root;
+            root = root.right;
+        }
+        return true;
+    }
+
+    public static void main(String[] args){
+        TreeNode t = new TreeNode(5);
+        t.left = new TreeNode(1);
+        t.right = new TreeNode(4);
+        t.right.left = new TreeNode(3);
+        t.right.right = new TreeNode(6);
+
+        System.out.println(isValidBST(t));
+    }
+}
