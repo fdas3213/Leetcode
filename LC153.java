c3c461f69ade13a5c9dc8ab5f1cd33cc5eb1a29c
fdas3213 <szx9404@gmail.com>
2019-04-11 10:12:55 -0700

    add two solutions

--- LC153.java
+++ LC153.java
@@ -1,13 +1,28 @@
 public class LC153 {
     public static int findMin(int[] nums){
         int low = 0, high = nums.length-1;
-        while (low <= high){
+        while (low < high){
             int mid = low + (high -low)/2;
-            if (nums[high] > nums[low]) high = mid-1;
-            else {
-                if (nums[mid] > nums[low]) low = mid+1;
-                else high = mid-1;
-            }
+            if (nums[mid] < nums[high]) high = mid;
+            else if (nums[mid] > nums[high]) low = mid+1;
+        }
+        return nums[low];
+    }
+
+    private static int Sol2(int[] nums){
+        /*
+        2 conditions:
+        (a) if rotated: if mid > 0 and nums[mid] < nums[mid-1], then return nums[mid]
+        (b) if not rotated: nums[0]
+         */
+        int low = 0, high = nums.length-1;
+        while(low < high){
+            int mid = low + (high - low)/2;
+            if(mid > 0 && nums[mid] < nums[mid-1]) return nums[mid];
+            //if mid element is greater than both ends, then search right hand side
+            if(nums[mid] >= nums[low] && nums[mid] > nums[high]) low = mid+1;
+            //else search left hand side
+            else high = mid-1;
         }
         return nums[low];
     }
@@ -15,7 +30,9 @@ public class LC153 {
     public static void main(String[] args){
         int[] t1 = {3,4,5,1,2};
         int[] t2 = {4,5,6,7,0,1,2};
+        int[] t3 = {3,1,2};
         System.out.println(findMin(t1));
         System.out.println(findMin(t2));
+        System.out.println(findMin(t3));
     }
 }
