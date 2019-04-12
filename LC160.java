7f8d73417426086476961909365f48316784817f
fdas3213 <szx9404@gmail.com>
2019-04-11 21:58:20 -0700

    LC160

--- LC160.java
+++ LC160.java
@@ -1,34 +1,33 @@
-/*
-Write a program to find the node at which the intersection of two singly linked lists begins.
-
-
-For example, the following two linked lists:
-
-A:          a1 → a2
-                   ↘
-                     c1 → c2 → c3
-                   ↗
-B:     b1 → b2 → b3
-begin to intersect at node c1.
-
-
-Notes:
-
-If the two linked lists have no intersection at all, return null.
-The linked lists must retain their original structure after the function returns.
-You may assume there are no cycles anywhere in the entire linked structure.
-Your code should preferably run in O(n) time and use only O(1) memory.
-
-
-
 public class LC160 {
     public static ListNode getIntersectionNode(ListNode headA, ListNode headB) {
         if (headA == null || headB == null) return null;
-
+        //find the shorter linkedlist
+        int l1 = 0, l2 = 0;
+        ListNode copyA = headA;
+        ListNode copyB = headB;
+        while(copyA != null){
+            copyA = copyA.next;
+            l1++;
+        }
+        while(copyB != null){
+            copyB = copyB.next;
+            l2++;
+        }
+        //make two linkedlist same length
+        if(l1 > l2){
+            for (int i =0; i < l1-l2; i++) headA = headA.next;
+        }else if (l1 < l2){
+            for (int i = 0; i < l2-l1;i++) headB = headB.next;
+        }
+        while(headA != headB){
+            headA = headA.next;
+            headB = headB.next;
+        }
+        return headA;
     }
     public static void main(String[] args){
-        ListNode l1 = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
-        ListNode l2 = new ListNode(3,new ListNode(1, new ListNode(3, new ListNode(4, new ListNode(5)))));
+        ListNode l1 = new ListNode(0, new ListNode(9, new ListNode(1, new ListNode(2, new ListNode(4)))));
+        ListNode l2 = new ListNode(3,new ListNode(2, new ListNode(4)));
+        ListNode.toString(getIntersectionNode(l1,l2));
     }
 }
- */
