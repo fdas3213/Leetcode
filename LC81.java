173aeb530cae5e0c00ae044f60a8ec55a7b17a3c
fdas3213 <szx9404@gmail.com>
2019-04-25 19:08:18 -0700

    changed mid to avoid overflow

--- LC81.java
+++ LC81.java
@@ -2,7 +2,7 @@ public class LC81 {
     public static boolean search(int[] nums, int target){
         int start = 0, end = nums.length -1;
         while (start <= end){
-            int mid = (end + start)/2;
+            int mid = start + (end - start)/2;
             if (nums[mid] == target) return true;
             while (start < mid && nums[start] == nums[mid]) start++;
             if (nums[start] <= nums[mid]){
