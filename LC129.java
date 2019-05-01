df27a94246a2a49f42e4cfc31e4f329a5a5b5a60
fdas3213 <szx9404@gmail.com>
2019-05-01 16:30:25 -0700

    added a levelorder traversal way

--- LC129.java
+++ LC129.java
@@ -1,3 +1,5 @@
+import java.util.LinkedList;
+
 public class LC129 {
     public static int sumNumbers(TreeNode root) {
         return sum(root, 0);
@@ -7,6 +9,31 @@ public class LC129 {
         if (root == null) return 0;
         if (root.left == null && root.right == null) return total*10+root.val;
         return sum(root.left, total*10+root.val) + sum(root.right, total*10+root.val);
+    }
+
+    public static int sol2(TreeNode root) {
+        //another way using level order traversal
+        if (root == null) return 0;
+        if (root.left == null && root.right == null) return root.val;
+        LinkedList<TreeNode> queue = new LinkedList<>();
+        LinkedList<Integer> sum = new LinkedList<>();
+        queue.offer(root);
+        sum.offer(root.val);
+        int total = 0;
+        while (!queue.isEmpty()){
+            TreeNode cur = queue.poll();
+            int curSum = sum.poll();
+            if (cur.left == null && cur.right == null) total += curSum;
+            if (cur.left != null) {
+                queue.offer(cur.left);
+                sum.offer(curSum*10 + cur.left.val);
+            }
+            if (cur.right != null) {
+                queue.offer(cur.right);
+                sum.offer(cur.right.val + curSum*10);
+            }
+        }
+        return total;
     }
 
     public static void main(String[] args){
