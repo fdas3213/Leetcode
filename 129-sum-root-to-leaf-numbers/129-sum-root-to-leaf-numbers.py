15347717beb20d61c3c56dafad1934fe2d34cf29
Shuzhe Xiao <szx9404@gmail.com>
2022-01-14 12:43:17 -0800

    Time: 32 ms (71.74%), Space: 14.4 MB (18.97%) - LeetHub

--- 129-sum-root-to-leaf-numbers/129-sum-root-to-leaf-numbers.py
+++ 129-sum-root-to-leaf-numbers/129-sum-root-to-leaf-numbers.py
@@ -8,27 +8,21 @@ class Solution:
     def sumNumbers(self, root: Optional[TreeNode]) -> int:
         if not root:
             return 0
-        queue = deque()
-        queue_sum = deque()
-        cur = root
-        queue.append(cur)
-        queue_sum.append(cur.val)
-        sum = 0
-        while queue:
-            size = len(queue)
-            for _ in range(size):
-                cur = queue.popleft()
-                cur_v = queue_sum.popleft()
-                if cur.left:
-                    queue.append(cur.left)
-                    queue_sum.append(cur_v*10+cur.left.val)
-                
-                if cur.right:
-                    queue.append(cur.right)
-                    queue_sum.append(cur_v*10+cur.right.val)
-                
-                if not cur.left and not cur.right:
-                    # add the path sum if we reach a leaf node
-                    sum += cur_v
         
-        return sum
+        def helper(root, curSum):
+            nonlocal totalSum
+            
+            if not root:
+                return
+            curSum = curSum * 10 + root.val
+            if not root.left and not root.right:
+                #at the leaf node
+                totalSum += curSum
+            else:
+                helper(root.left, curSum)
+                helper(root.right, curSum)
+        
+        totalSum = 0
+        helper(root, 0)
+        
+        return totalSum
