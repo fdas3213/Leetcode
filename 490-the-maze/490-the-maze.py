9ceefd18ebd0fc067d4913221869f660fd154425
Shuzhe Xiao <szx9404@gmail.com>
2022-01-22 18:21:04 -0800

    Time: 291 ms (66.56%), Space: 16.4 MB (26.27%) - LeetHub

---
+++ 490-the-maze/490-the-maze.py
@@ -0,0 +1,36 @@
+class Solution:
+    
+    def hasPath(self, maze: List[List[int]], start: List[int], destination: List[int]) -> bool:
+        m, n = len(maze), len(maze[0])
+        self.dirs = [[1,0],[-1,0],[0,1],[0,-1]]
+    
+        def dfs(i, j, m, n, visited):
+            if (i,j) in visited:
+                return False
+            
+            visited.add((i,j))
+            
+            if [i,j] == destination:
+                return True
+            
+            for d in self.dirs:
+                x = i + d[0]
+                y = j + d[1]
+                
+                while x>=0 and y>=0 and x<m and y< n and maze[x][y]==0:
+                    x += d[0]
+                    y += d[1]
+                    
+                #Because we overshoot by an extra cell due to while loop becoming invalid, so we reduce it be 1 to make the cell valid again.
+                
+                x -= d[0]
+                y -= d[1]
+                
+                if dfs(x, y, m, n, visited):
+                    return True
+            
+            return False   
+        
+        s_i, s_j = start[0], start[1]
+        
+        return dfs(s_i, s_j, m,n, set())
