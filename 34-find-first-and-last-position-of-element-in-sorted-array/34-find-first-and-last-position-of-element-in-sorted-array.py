671daba8ff0b8cc7f6320c7febb479ede177bc75
Shuzhe Xiao <szx9404@gmail.com>
2022-01-24 22:33:51 -0800

    Time: 121 ms (31.04%), Space: 15.4 MB (84.03%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,24 +1,26 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        if len(nums)==0:
+        if not nums or target>nums[-1] or target<nums[0]:
             return [-1,-1]
         
-        start, end = 0, len(nums)-1
+        def bisect_left(nums, target):
+            start, end = 0, len(nums)-1
+            while start < end:
+                mid = start + (end-start)//2
+                if nums[mid] < target:
+                    start = mid +1
+                else:
+                    end = mid
+            return start if nums[start]==target else -1
         
-        while start<=end:
-            mid = start+(end-start)//2
-            if target<nums[mid]:
-                end=mid-1
-            elif target>nums[mid]:
-                start=mid+1
-            else:
-                left, right = mid, mid
-                while left>=0 and nums[left]==target:
-                    left-=1
-                while right<len(nums) and nums[right]==target:
-                    right+=1
-                return [left+1,right-1]
+        def bisect_right(nums, target):
+            start, end = 0, len(nums)-1
+            while start < end:
+                mid = start + (end-start)//2+1
+                if nums[mid] > target:
+                    end = mid -1
+                else:
+                    start = mid
+            return start if nums[start]==target else -1
         
-        
-        return [-1,-1]
-                
+        return [bisect_left(nums,target), bisect_right(nums, target)]
