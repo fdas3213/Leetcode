fbc780571f4961944fc9fe8b990a08f9b0459aa8
Shuzhe Xiao <szx9404@gmail.com>
2022-01-26 14:07:02 -0800

    Time: 32 ms (75.97%), Space: 14.3 MB (47.23%) - LeetHub

--- 362-design-hit-counter/362-design-hit-counter.py
+++ 362-design-hit-counter/362-design-hit-counter.py
@@ -1,15 +1,25 @@
 class HitCounter:
 
     def __init__(self):
+        #Using queue: space and time complexity both O(N)
         self.queue = deque()
         self.th = 300
+        self.count = 0
+        
     def hit(self, timestamp: int) -> None:
-        self.queue.append(timestamp)
+        if self.queue and self.queue[-1][0]==timestamp:
+            prev_count = self.queue.pop()[1]
+            self.queue.append((timestamp, prev_count+1))
+            self.count -= prev_count
+        else:
+            self.queue.append((timestamp,1))
+        self.count += self.queue[-1][1]
 
     def getHits(self, timestamp: int) -> int:
-        while self.queue and timestamp-self.queue[0]>=self.th:
+        while self.queue and timestamp-self.queue[0][0]>=self.th:
+            self.count -= self.queue[0][1]
             self.queue.popleft()
-        return len(self.queue)
+        return self.count
 
 
 # Your HitCounter object will be instantiated and called as such:
