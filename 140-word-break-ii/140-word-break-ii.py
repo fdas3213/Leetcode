bd4aa5e1b6c5ce199e09fc79ec7abcfe9dc03fa6
Shuzhe Xiao <szx9404@gmail.com>
2022-02-14 17:00:28 -0800

    Time: 39 ms (54.59%), Space: 13.9 MB (80.51%) - LeetHub

--- 140-word-break-ii/140-word-break-ii.py
+++ 140-word-break-ii/140-word-break-ii.py
@@ -1,29 +1,27 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
-        segments = deque([s])
-        res = deque([""])
+        segments = deque([(s, "")])
         ans = []
         
         while segments:
             l = len(segments)
             for i in range(l):
-                curstr = segments.popleft()
-                cur_sol = res.popleft()
+                curstr, cursol = segments.popleft()
                 #add solution to the output when segment is empty
                 if len(curstr) == 0:
-                    ans.append(cur_sol)
+                    ans.append(cursol)
                     continue
                 for word in wordDict:
                     if curstr.startswith(word):
                         #find the index of word in curstr, and add rest of curstr into segments
                         word_len = len(word)
                         newstr = curstr[word_len:]
-                        segments.append(newstr)
                         
                         # add word to the solution
-                        new_sol = cur_sol+word
+                        new_sol = cursol+word
                         if len(newstr)!=0:
                             new_sol += " "
-                        res.append(new_sol)
+                            
+                        segments.append((newstr, new_sol))
         
         return ans
