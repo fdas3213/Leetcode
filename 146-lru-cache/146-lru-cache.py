5b190932f1be0496221829be2ed542e54d53395e
Shuzhe Xiao <szx9404@gmail.com>
2022-02-22 14:46:40 -0800

    Time: 1293 ms (41.01%), Space: 76.5 MB (22.87%) - LeetHub

--- 146-lru-cache/146-lru-cache.py
+++ 146-lru-cache/146-lru-cache.py
@@ -1,67 +1,62 @@
 class Node:
-    def __init__(self, key=0, val=0, pre=None, next=None):
+    def __init__(self, key, val, pre=None, next=None):
         self.key = key
         self.val = val
         self.pre = pre
         self.next = next
-    
+        
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.kv_pair = {}
+        self.capacity = capacity
         self.head = Node(-1, -1)
         self.tail = Node(-1, -1)
-        self.head.pre = self.tail
         self.head.next = self.tail
         self.tail.pre = self.head
-        self.tail.next = self.head
-        self.capacity = capacity
+        self.key_map = defaultdict(Node)
     
-    def add_node_to_tail(self, node: Node) -> None:
-        #first move pre and next pointer of the node
-        if node.next:
-            next_node = node.next
-            node.pre.next = next_node
-            next_node.pre = node.pre
-        
-        #then move the node to the front of the tail node
-        prev = self.tail.pre
-        prev.next = node
-        node.pre = prev
-        node.next = self.tail
-        self.tail.pre = node
-        
+    def _add(self, node):
+        prev_node = self.head.next
+        self.head.next = node
+        node.pre = self.head
+        node.next = prev_node
+        prev_node.pre = node
+    
+    def _update(self, node:Node, val:int):
+        node.val = val
+        node.next.pre = node.pre
+        node.pre.next = node.next
+        self._add(node)
+    
+    def _remove(self):
+        node = self.tail.pre
+        self.tail.pre = node.pre
+        node.pre.next = self.tail
+        del self.key_map[node.key]
         
     def get(self, key: int) -> int:
-        if key not in self.kv_pair:
-            return -1
-        node = self.kv_pair[key]
-        self.add_node_to_tail(node)
-        return node.val
-        
+        val = -1
+        if key in self.key_map:
+            curnode = self.key_map[key]
+            val = curnode.val
+            self._update(curnode, val)
+        return val
 
     def put(self, key: int, value: int) -> None:
-        #This essentially works like a Queue, where we add from tail, and pop from head
-            
-        #if key does not already exist, we create a new key
-        if key not in self.kv_pair:
-            node = Node(key, value)
+        # add the key into the map
+        if key in self.key_map:
+            node = self.key_map[key]
+            self._update(node, value)
         else:
-            node = self.kv_pair[key]
-            node.val = value
-
-        self.add_node_to_tail(node)
-        
-        #add the new node/newly updated node to the dictionary
-        self.kv_pair[key] = node
-
-        #when the LRU cache is at full capacity, we evict the key next to head
-        if len(self.kv_pair)>self.capacity:
-            n = self.head.next
-            del self.kv_pair[n.key]
-            self.head.next = n.next
-            n.next.pre = self.head
+            node = Node(key=key, val=value)
+            self.key_map[key] = node
+            self._add(node)
             
+        # if number of nodes exceed limit, remove the least recently used key
+        if len(self.key_map) > self.capacity:
+            self._remove()
+             
+
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
