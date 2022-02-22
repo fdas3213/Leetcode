fe591088e784c566c90bfb620c50f9ecb0f80bec
Shuzhe Xiao <szx9404@gmail.com>
2022-02-22 13:56:29 -0800

    Time: 380 ms (64.47%), Space: 16.4 MB (91.25%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -1,25 +1,23 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        if len(grid) == 0 or len(grid[0]) == 0:
-            return 0
         m, n = len(grid), len(grid[0])
+        dirs = [[1,0],[-1,0],[0,1],[0,-1]]
         count = 0
+        
+        def dfs(i:int, j:int):
+            if i<0 or j<0 or i>=m or j>=n or grid[i][j]!='1':
+                return
+            
+            grid[i][j] = "*"
+            for d in dirs:
+                x = i+d[0]
+                y = j+d[1]
+                dfs(x,y)
+        
         for i in range(m):
             for j in range(n):
-                # if grid[i][j] is 1, then we change the value of all connected 1 to 0, and add 1 to count  
-                if grid[i][j] == "1":
+                if grid[i][j]=='1':
                     count += 1
-                    self.dfs(grid, i, j, m, n)
+                    dfs(i,j)
         
         return count
-    
-    def dfs(self, grid, i, j,m,n):
-        if i<0 or j<0 or i>=m or j>=n or grid[i][j]!="1":
-            return 
-        
-        grid[i][j] = "0"
-        self.dfs(grid, i+1, j, m, n)
-        self.dfs(grid, i-1, j, m, n)
-        self.dfs(grid, i, j+1, m,n)
-        self.dfs(grid,i,j-1,m,n)
-        
