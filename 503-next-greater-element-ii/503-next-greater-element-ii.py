68e4d3190ef1848f867017469dc98afde41b5b3f
Shuzhe Xiao <szx9404@gmail.com>
2022-02-25 16:15:24 -0800

    Time: 216 ms (89.10%), Space: 16.5 MB (8.58%) - LeetHub

---
+++ 503-next-greater-element-ii/503-next-greater-element-ii.py
@@ -0,0 +1,27 @@
+class Solution:
+    def nextGreaterElements(self, nums: List[int]) -> List[int]:
+        digit_map = {}
+        max_num = max(nums)
+        max_index = -1
+        for idx, n in enumerate(nums):
+            if n == max_num:
+                max_index = idx
+        
+        n = len(nums)
+        stack = []
+        for i in range(max_index+1, max_index+1+n, 1):
+            cur_index = i%n
+            cur_num = nums[cur_index]
+            while stack and stack[-1][-1] < cur_num:
+                digit_map[stack.pop()] = cur_num
+            stack.append((cur_index, cur_num))
+        
+        while stack:
+            digit_map[stack.pop()] = -1
+        
+        res = []
+        for i, n in enumerate(nums):
+            res.append(digit_map[(i,n)])
+        
+        return res
+        
