925eb61fbee444af3150273465fe50798475c0de
Shuzhe Xiao <szx9404@gmail.com>
2022-03-06 20:43:56 -0800

    Time: 757 ms (31.82%), Space: 61.6 MB (48.04%) - LeetHub

--- 380-insert-delete-getrandom-o1/380-insert-delete-getrandom-o1.py
+++ 380-insert-delete-getrandom-o1/380-insert-delete-getrandom-o1.py
@@ -2,40 +2,40 @@ class RandomizedSet:
 
     def __init__(self):
         # val map to the position it gets added to the hashmap
-        self.randomSet = defaultdict(int)
-        # position map to val
         self.valMap = defaultdict(int)
+        # position map to val
+        self.posMap = defaultdict(int)
         self.count = 0
         
     def insert(self, val: int) -> bool:
-        if val in self.randomSet:
+        if val in self.valMap:
             return False
-        self.randomSet[val] = self.count
-        self.valMap[self.count] = val
+        self.valMap[val] = self.count
+        self.posMap[self.count] = val
         self.count += 1
         return True
 
     def remove(self, val: int) -> bool:
-        if val not in self.randomSet:
+        if val not in self.valMap:
             return False
-        pos = self.randomSet[val]
-        del self.randomSet[val]
-        del self.valMap[pos]
+        pos = self.valMap[val]
+        del self.valMap[val]
+        del self.posMap[pos]
         
         self.count -= 1
-        if not self.valMap:
+        if not self.posMap:
             return True
         
-        last_element = self.valMap[self.count]
-        self.valMap[pos] = last_element
-        self.randomSet[last_element] = pos
+        last_element = self.posMap[self.count]
+        self.posMap[pos] = last_element
+        self.valMap[last_element] = pos
         
         
         return True
 
     def getRandom(self) -> int:
         pos = random.randint(0, self.count-1)
-        return self.valMap[pos]
+        return self.posMap[pos]
 
 
 # Your RandomizedSet object will be instantiated and called as such:
