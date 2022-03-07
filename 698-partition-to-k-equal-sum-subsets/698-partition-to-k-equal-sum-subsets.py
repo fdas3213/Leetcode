a7078592252a791fcec277dec7f67f0521bee85b
Shuzhe Xiao <szx9404@gmail.com>
2022-03-07 11:48:25 -0800

    Time: 413 ms (51.39%), Space: 14.1 MB (73.41%) - LeetHub

--- 698-partition-to-k-equal-sum-subsets/698-partition-to-k-equal-sum-subsets.py
+++ 698-partition-to-k-equal-sum-subsets/698-partition-to-k-equal-sum-subsets.py
@@ -1,38 +1,42 @@
 class Solution:
     def canPartitionKSubsets(self, nums: List[int], k: int) -> bool:
-        #check if the array sum can be evenly divided into k parts
-        arr_sum = sum(nums)
-        if arr_sum%k!=0:
+        total = sum(nums)
+        if total%k!=0:
             return False
-        
-        n = len(nums)
-        used = [False for _ in range(n)]
-        target_sum = arr_sum//k
+        subset_sum = total//k
+        # use the index of each element to indicate whether an element has been used or not
+        used = set()
         
         nums.sort(reverse=True)
         
-        def backtrack(pos:int, count:int, cursum:int):
-            #we made k - 1 subsets with target sum and the last subset will also have target sum.
-            if count == k-1:
+        def backtrack(start:int, cursum:int, subset_count:int):
+            """
+            1. cursum == subset sum
+            2. collected k subsets
+            3. used all elements
+            """
+            if subset_count==k-1:
                 return True
             
-            if cursum==target_sum:
-                return backtrack(0, count+1, 0)
-                
-            for i in range(pos, n):
-                # avoid duplicated backtracking
-                if i > 0 and not used[i-1] and nums[i] == nums[i-1]:
+            if cursum==subset_sum:
+                return backtrack(0, 0, subset_count+1)
+            
+            #backtrack
+            for i in range(start, len(nums)):
+                """
+                avoid dup: if current element==previous element, and 
+                previous element not used, then current element should not be used
+                """
+                if i>0 and i-1 not in used and nums[i]==nums[i-1]:
                     continue
-                    
-                if not used[i] and cursum+nums[i]<=target_sum:
-                    used[i] = True
-                    if backtrack(i+1, count, cursum+nums[i]):
+                
+                if i not in used and cursum+nums[i]<=subset_sum:
+                    used.add(i)
+                    if backtrack(i+1, cursum+nums[i], subset_count):
                         return True
-                    used[i] = False
+                    used.remove(i)
             
             return False
-            
+        
         return backtrack(0, 0, 0)
                 
-                
-            
