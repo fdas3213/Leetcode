34c3b601148e40aba8ff1cc5ef539db5112318ef
Shuzhe Xiao <szx9404@gmail.com>
2022-03-15 17:39:06 -0700

    Time: 406 ms (39.44%), Space: 18.6 MB (53.87%) - LeetHub

--- 528-random-pick-with-weight/528-random-pick-with-weight.py
+++ 528-random-pick-with-weight/528-random-pick-with-weight.py
@@ -1,33 +1,27 @@
 class Solution:
 
     def __init__(self, w: List[int]):
-        self.cum_sum = 0
-        total = 0
-        self.cum_weights = []
-        
+        self.weights = []
+        cursum = 0
         for weight in w:
-            total += weight
-            self.cum_weights.append(total)
+            cursum += weight
+            self.weights.append(cursum)
+        self.cumsum = cursum
         
-        self.cum_sum = total
-
     def pickIndex(self) -> int:
-        if not self.cum_weights:
-            return -1
-        
-        r = random.randint(1, self.cum_sum)
-        
-        left, right = 0, len(self.cum_weights)-1
+        r = random.randint(1, self.cumsum)
+        left, right = 0, len(self.weights)-1
         while left<=right:
-            mid = left+(right-left)//2
-            if self.cum_weights[mid]==r:
+            mid = left + (right-left)//2
+            if self.weights[mid]==r:
                 return mid
-            elif r>self.cum_weights[mid]:
-                left=mid+1
-            else:
+            elif self.weights[mid]>r:
                 right = mid-1
+            else:
+                left = mid+1
+        
         return left
-
+        
 
 # Your Solution object will be instantiated and called as such:
 # obj = Solution(w)
