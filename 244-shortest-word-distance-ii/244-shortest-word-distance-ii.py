c2807dc8de2a546cda1501bcae221885a25f1a00
Shuzhe Xiao <szx9404@gmail.com>
2022-03-20 21:05:34 -0700

    Time: 100 ms (91.47%), Space: 21.9 MB (24.67%) - LeetHub

--- 244-shortest-word-distance-ii/244-shortest-word-distance-ii.py
+++ 244-shortest-word-distance-ii/244-shortest-word-distance-ii.py
@@ -1,29 +1,24 @@
 class WordDistance:
 
     def __init__(self, wordsDict: List[str]):
-        self.word_index = defaultdict(list)
-        self.n = 0
+        self.wordMap = defaultdict(list)
         for index, word in enumerate(wordsDict):
-            self.word_index[word].append(index)
-            self.n += 1
-        
+            self.wordMap[word].append(index)
+
     def shortest(self, word1: str, word2: str) -> int:
-        w1_list = self.word_index[word1]
-        w2_list = self.word_index[word2]
-        min_dist = self.n
-        if len(w1_list)==len(w2_list)==1:
-            return abs(w1_list[0]-w2_list[0])
-        
-        p1, p2 = 0,0
-        while p1<len(w1_list) and p2<len(w2_list):
-            #"and" operator is used because when one pointer hits the end, incrementing the other pointer would only create a bigger difference
-            min_dist = min(min_dist, abs(w1_list[p1]-w2_list[p2]))
-            if w1_list[p1]<w2_list[p2]:
-                p1 += 1
+        l1, l2 = len(self.wordMap[word1]), len(self.wordMap[word2])
+        p1, p2 = 0, 0
+        curMin = abs(self.wordMap[word1][p1]-self.wordMap[word2][p2])
+        while p1<l1 and p2<l2:
+            m1, m2 = self.wordMap[word1][p1], self.wordMap[word2][p2]
+            curMin = min(curMin, abs(m1-m2))
+            if m1<=m2:
+                p1+=1
             else:
-                p2 += 1
-        return min_dist
-                    
+                p2+=1
+        return curMin
+        
+
 
 # Your WordDistance object will be instantiated and called as such:
 # obj = WordDistance(wordsDict)
