fd381a5baae65683620b06de1d2e8f2adecdf606
Shuzhe Xiao <szx9404@gmail.com>
2022-03-20 21:35:15 -0700

    Time: 72 ms (50.75%), Space: 16.4 MB (65.18%) - LeetHub

--- 272-closest-binary-search-tree-value-ii/272-closest-binary-search-tree-value-ii.py
+++ 272-closest-binary-search-tree-value-ii/272-closest-binary-search-tree-value-ii.py
@@ -6,23 +6,50 @@
 #         self.right = right
 class Solution:
     def closestKValues(self, root: Optional[TreeNode], target: float, k: int) -> List[int]:
-        #O(K) to O(N): inorder traversal with a deque. When deque reaches size of k, and distance btw target and new element is larger than dist(first deque element, target), then we can simply stop traversal because future answers will be even further away
-        queue = deque([])
+        #inorder traversal
+        nodes = []
+        stack = []
+        count = 0
+        closest = root.val
+        while stack or root:
+            while root:
+                stack.append(root)
+                root = root.left
+            root = stack.pop()
+            nodes.append(root.val)
+            count += 1
+            if abs(root.val-target)<abs(closest-target):
+                closest = root.val
+            root = root.right
         
-        def inorder(root):
-            if not root:
-                return
-            
-            inorder(root.left)
-            if len(queue)==k:
-                if abs(root.val-target)<abs(queue[0]-target):
-                    queue.popleft()
+        closestIndex = 0
+        for index, item in enumerate(nodes):
+            if item==closest:
+                closestIndex = index
+        res = [closest]
+        left, right = closestIndex-1, closestIndex+1
+        while k>1:
+            if left>=0 and right<count:
+                if abs(nodes[left]-target)<abs(nodes[right]-target):
+                    res.append(nodes[left])
+                    left -= 1
                 else:
-                    return
-            queue.append(root.val)
-            inorder(root.right)
-        
-        inorder(root)
-        
-        return [v for v in queue]
+                    res.append(nodes[right])
+                    right += 1
+            elif left>=0:
+                res.append(nodes[left])
+                left -= 1
+            elif right<count:
+                res.append(nodes[right])
+                right += 1
             
+            k-=1
+        
+        return res
+                    
+        
+        return res
+            
+        
+            
+        
