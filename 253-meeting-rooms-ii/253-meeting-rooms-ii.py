5805cf5896aa790aaa43543d3eec3639309efa7e
Shuzhe Xiao <szx9404@gmail.com>
2022-03-25 11:17:28 -0700

    Time: 122 ms (39.95%), Space: 17.7 MB (19.78%) - LeetHub

--- 253-meeting-rooms-ii/253-meeting-rooms-ii.py
+++ 253-meeting-rooms-ii/253-meeting-rooms-ii.py
@@ -1,21 +1,15 @@
 class Solution:
-    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-        """
-        Idea: compute number of non-overlapping intervals
-        step 1. sort intervals based on start time and end time
-        step 2. we add 1 if the current start time is less than the minimum end time, otherwise we increment end time pointer by 1
-        
-        O(Nlog(N)) for sorting, and O(N) for iterating
-        """
-        
-        startTime = sorted([interval[0] for interval in intervals])
-        endTime = sorted([interval[1] for interval in intervals])
-        count, end = 0, 0
-        for start in startTime:
-            if start<endTime[end]:
-                count += 1
+    def minMeetingRooms(self, intervals: List[List[int]]) -> int:        
+        minHeap = []
+        intervals = sorted(intervals, key=lambda x:x[0])
+        for interval in intervals:
+            if not minHeap:
+                heappush(minHeap, (interval[1], interval[0]))
+                continue
+            
+            if interval[0]<minHeap[0][0]:
+                heappush(minHeap, (interval[1], interval[0]))
             else:
-                #endTime[end] always points to the earliest endTime, so if new meeting start time is earlier than the
-                #earliest endTime, then of course need to add 1 meeting room
-                end += 1
-        return count
+                heappushpop(minHeap, (interval[1], interval[0]))
+        
+        return len(minHeap)
