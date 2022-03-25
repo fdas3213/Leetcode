a722c241cae3a8a807f3588abb22541376365bbd
Shuzhe Xiao <szx9404@gmail.com>
2022-03-24 17:04:13 -0700

    Time: 32 ms (98.60%), Space: 14.2 MB (33.44%) - LeetHub

--- 139-word-break/139-word-break.py
+++ 139-word-break/139-word-break.py
@@ -1,22 +1,20 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        queue = deque([s])
         used = set()
-        while queue:
-            size = len(queue)
-            for i in range(size):
-                cur = queue.popleft()
-                for word in wordDict:
-                    if word==cur:
-                        return True
-                    if cur.find(word)==0:
-                        nxt = cur[len(word):]
-                        if nxt not in used:
-                            used.add(nxt)
-                            queue.append(nxt)
-                    # if cur.find(word) != -1:
-                    #     index = cur.find(word)
-                    #     nxt = cur[:index]+cur[index+len(word):]
-                    #     queue.append(nxt)
         
-        return False
+        def dfs(cur:str):
+            # terminal condition: the input has been completely segmented 
+            if cur=="":
+                return True
+            
+            for word in wordDict:
+                if cur.find(word)==0:
+                    nxt = cur[len(word):]
+                    if nxt not in used:
+                        used.add(nxt)
+                        if dfs(nxt):
+                            return True
+            
+            return False
+        
+        return dfs(s)
