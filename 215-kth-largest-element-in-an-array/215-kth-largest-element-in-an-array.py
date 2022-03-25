e6ea9166e8aaf8f32cecd1cb6cb2153653e2bdde
Shuzhe Xiao <szx9404@gmail.com>
2022-03-25 15:16:06 -0700

    Time: 1623 ms (11.91%), Space: 14.8 MB (77.87%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.py
@@ -1,12 +1,36 @@
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        #priority queue: o(nlog(k))
         
-        minHeap = []
-        for n in nums[:k]:
-            heappush(minHeap, n)
+        def partition(left, right):
+            i, j = left, right
+            #choose the first element as the pivot element
+            pivot = nums[left]
+            
+            while i<j:
+                #place all elements less than pivot to the left of pivot
+                while i<right and nums[i]<=pivot:
+                    i += 1
+                #place all elements greater than pivot to the right of pivot
+                while j>0 and nums[j]>pivot:
+                    j -= 1
+                
+                if i<j:
+                    nums[i], nums[j] = nums[j], nums[i]
+            
+            #swap with pivot
+            nums[j], nums[left] = nums[left], nums[j]
+            return j
         
-        for n in nums[k:]:
-            heappushpop(minHeap, n)
+        left, right = 0, len(nums)-1
+        #kth largest is the (l-k)th smallest element. [1,2,3,4,5], 2nd largest element is the 3rd smallest element
+        k = len(nums)-k
+        while left<right:
+            pivotIndex = partition(left, right)
+            if pivotIndex==k:
+                return nums[k]
+            elif pivotIndex>k:
+                right = pivotIndex-1
+            else:
+                left = pivotIndex+1
         
-        return minHeap[0]
+        return nums[k]
