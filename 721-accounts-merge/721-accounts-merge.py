109114503794e3b8f8230a6b9c522535eff784b2
Shuzhe Xiao <szx9404@gmail.com>
2022-03-26 18:03:48 -0700

    Time: 208 ms (91.15%), Space: 23.1 MB (29.71%) - LeetHub

--- 721-accounts-merge/721-accounts-merge.py
+++ 721-accounts-merge/721-accounts-merge.py
@@ -2,46 +2,36 @@ class Solution:
     def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
         graph = defaultdict(list)
         
-        #use the first email to build a graph
-        # add edge between first email to all other emails in the account
         for account in accounts:
             first_email = account[1]
             
             for email in account[2:]:
-                graph[first_email].append(email)
                 graph[email].append(first_email)
+                graph[first_email].append(email)
         
-        #initialize visited set
         visited = set()
         
-        #dfs traverse
-        def dfs(account, first_email):
-            if first_email not in graph:
-                return
-            
-            for email in graph[first_email]:
-                if email not in visited:
-                    account.append(email)
-                    visited.add(email)
-                    dfs(account, email)
+        def dfs(email, mergedList):
+            for neighbor in graph[email]:
+                if neighbor not in visited:
+                    visited.add(neighbor)
+                    mergedList.append(neighbor)
+                    dfs(neighbor, mergedList)
         
-        #traverse through the input array again
-        res = []
+        ans = []
+        
         for account in accounts:
-            name, first_email = account[0],account[1]
+            name = account[0]
+            first_email = account[1]
+            if first_email in visited:
+                # avoid adding duplicate
+                continue
+            curList = [name]
+            curList.append(first_email)
+            visited.add(first_email)
+            dfs(first_email, curList)
+            curList[1:] = sorted(curList[1:])
+            ans.append(curList)
             
-            #if this email is already visited, then no need to continue
-            if first_email not in visited:
-                mergedAccount = []
-                mergedAccount.append(name)
-                mergedAccount.append(first_email)
-                visited.add(first_email)
-                dfs(mergedAccount, first_email)
-
-                mergedAccount[1:] = sorted(mergedAccount[1:])
-                res.append(mergedAccount)
-        
-        return res
-        
-            
-            
+        return ans
+    
