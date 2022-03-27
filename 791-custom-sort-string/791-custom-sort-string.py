1ff718f9ebf3c72ffc277220839399bf6383092c
Shuzhe Xiao <szx9404@gmail.com>
2022-03-27 14:31:41 -0700

    Time: 51 ms (33.43%), Space: 13.9 MB (22.14%) - LeetHub

--- 791-custom-sort-string/791-custom-sort-string.py
+++ 791-custom-sort-string/791-custom-sort-string.py
@@ -1,18 +1,7 @@
 class Solution:
     def customSortString(self, order: str, s: str) -> str:
         charMap = defaultdict(int)
-        
         for index, char in enumerate(order):
             charMap[char] = index
         
-        orders = []
-        for char in s:
-            if char not in charMap:
-                orders.append((-1, char))
-                continue
-            pos = charMap[char]
-            orders.append((pos, char))
-            
-        return "".join([tup[1] for tup in sorted(orders, key=lambda x: x[0])])
-            
-        
+        return "".join(sorted(s, key=lambda x: charMap[x]))
