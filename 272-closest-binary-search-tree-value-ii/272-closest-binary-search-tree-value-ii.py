6601fec084b10a23cf62ef868f6c3ee037171edd
Shuzhe Xiao <szx9404@gmail.com>
2022-04-14 22:34:14 -0700

    Time: 49 ms (91.63%), Space: 16.2 MB (89.96%) - LeetHub

--- 272-closest-binary-search-tree-value-ii/272-closest-binary-search-tree-value-ii.py
+++ 272-closest-binary-search-tree-value-ii/272-closest-binary-search-tree-value-ii.py
@@ -6,50 +6,26 @@
 #         self.right = right
 class Solution:
     def closestKValues(self, root: Optional[TreeNode], target: float, k: int) -> List[int]:
+        res = []
+        if not root:
+            return res
+        
         #inorder traversal
-        nodes = []
         stack = []
-        count = 0
-        closest = root.val
-        while stack or root:
+        while root or stack:
             while root:
                 stack.append(root)
                 root = root.left
+            
             root = stack.pop()
-            nodes.append(root.val)
-            count += 1
-            if abs(root.val-target)<abs(closest-target):
-                closest = root.val
+            if len(res)<k:
+                res.append(root.val)
+            elif abs(target-root.val)<abs(target-res[0]):
+                res.pop(0)
+                res.append(root.val)
+            elif abs(target-root.val)>=abs(target-res[0]):
+                return res
+                
             root = root.right
         
-        closestIndex = 0
-        for index, item in enumerate(nodes):
-            if item==closest:
-                closestIndex = index
-        res = [closest]
-        left, right = closestIndex-1, closestIndex+1
-        while k>1:
-            if left>=0 and right<count:
-                if abs(nodes[left]-target)<abs(nodes[right]-target):
-                    res.append(nodes[left])
-                    left -= 1
-                else:
-                    res.append(nodes[right])
-                    right += 1
-            elif left>=0:
-                res.append(nodes[left])
-                left -= 1
-            elif right<count:
-                res.append(nodes[right])
-                right += 1
-            
-            k-=1
-        
         return res
-                    
-        
-        return res
-            
-        
-            
-        
