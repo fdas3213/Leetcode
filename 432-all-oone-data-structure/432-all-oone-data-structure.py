b87bba9c592d945c35a34f037f5dad5d23201980
Shuzhe Xiao <szx9404@gmail.com>
2022-04-15 12:20:43 -0700

    Time: 223 ms (82.34%), Space: 32.3 MB (10.32%) - LeetHub

--- 432-all-oone-data-structure/432-all-oone-data-structure.py
+++ 432-all-oone-data-structure/432-all-oone-data-structure.py
@@ -1,116 +1,101 @@
 class Node:
-    def __init__(self):
-        #val is a bag of words 
-        self.keyset = set()
-        self.pre = None
-        self.next = None
-    
-    def add_key(self, key):
-        self.keyset.add(key)
+    def __init__(self, val, cnt=0, pre=None, next=None):
+        self.val = val
+        self.cnt = cnt
+        self.pre = pre
+        self.next = next
         
-    def remove_key(self, key):
-        self.keyset.remove(key)
-        
-    def get_key(self):
-        if self.keyset:
-            result = self.keyset.pop()
-            self.add_key(result)
-            return result
-        else:
-            return None
-    
-    def is_empty(self):
-        return len(self.keyset)==0
-    
 class AllOne:
 
     def __init__(self):
-        #{1: N(["a", "b"]), 3:N(["abc", "def"])}
-        self.node_freq = defaultdict(Node)
-        #{"a":1, "b":1,"abc":3, "def":3}
-        self.key_count = defaultdict(int)
-        self.head = Node()
-        self.tail = Node()
+        self.head = Node(-1)
+        self.tail = Node(-1)
         self.head.next = self.tail
         self.tail.pre = self.head
-        # self.node_freq[0] = self.head
-    
-    def _insert_after(self, node):
-        newnode = Node()
-        temp = node.next
-        newnode.next, temp.pre = temp, newnode
-        newnode.pre, node.next = node, newnode
-        return newnode
-    
-    def _insert_before(self, node):
-        return self._insert_after(node.pre)
+        #{str: node}
+        self.keyMap = defaultdict(Node)
+        #{cnt: list of strings with the same count}
+        self.cntMap = defaultdict(list)
+        # self.maxCnt = -1
+        # self.minCnt = float("inf")
     
     def _insert(self, key):
-        if 1 not in self.node_freq:
-            newnode = self._insert_after(self.head)
-            newnode.add_key(key)
-            self.node_freq[1] = newnode
-        else:
-            node = self.node_freq[1]
-            node.add_key(key)
-            self.node_freq[1] = node
-        self.key_count[key] = 1
-    
+        # insert a new key as the node next to the head node
+        node = Node(key, 1)
+        nxt = self.head.next
+        nxt.pre, node.next = node, nxt
+        self.head.next, node.pre = node, self.head
+        # add the key to cntMap 
+        self.cntMap[1].append(key)
+        # add the key to keyMap
+        self.keyMap[key] = node
+        
+        
     def _remove(self, node):
-        prev = node.pre
-        n = node.next
-        prev.next = n
-        n.pre = prev
+        #removes a node from the DList 
+        prev, nxt = node.pre, node.next
+        prev.next = nxt
+        nxt.pre = prev
     
-    def _update(self, key, operation):
-        count = self.key_count[key]
-        newCount = count+1 if operation=='inc' else count-1
-        # this is the node before update
-        prevNode = self.node_freq[count]
-        prevNode.remove_key(key)
-        
-        if newCount not in self.node_freq:
-            self.node_freq[newCount] = self._insert_after(prevNode) if operation=='inc' else self._insert_before(prevNode)
-            self.node_freq[newCount].add_key(key)
-        else:
-            node = self.node_freq[newCount]
-            node.add_key(key)
-            self.node_freq[newCount] = node
-        
-        if prevNode.is_empty():
-            self._remove(prevNode)
-            del self.node_freq[count]
-        else:
-            self.node_freq[count] = prevNode
-                
-        self.key_count[key] = newCount
-
     def inc(self, key: str) -> None:
-        if key not in self.key_count:
+        if key not in self.keyMap:
             self._insert(key)
         else:
-            self._update(key, 'inc')
-
+            #increment the count
+            prevCnt = self.keyMap[key].cnt
+            self.keyMap[key].cnt += 1
+            # remove the node from previous count list
+            self.cntMap[prevCnt].remove(key)
+            if not self.cntMap[prevCnt]:
+                del self.cntMap[prevCnt]
+            # add the node to new count list
+            self.cntMap[prevCnt+1].append(key)
+        
+        # print("function: inc", self.cntMap)
+            
     def dec(self, key: str) -> None:
-        # remove the key from key_count if its frequency is 0
-        count = self.key_count[key]
-        newCount = count-1
-        if newCount==0:
-            del self.key_count[key]
-            prevNode = self.node_freq[count]
-            prevNode.remove_key(key)
-            if prevNode.is_empty():
-                self._remove(prevNode)
-                del self.node_freq[count]
+        # check if the key exists or not
+        if key not in self.keyMap:
+            return ""
+        
+        cnt = self.keyMap[key].cnt
+        if cnt==1:
+            #remove the key from both hashmap
+            del self.keyMap[key]
+            self.cntMap[cnt].remove(key)
         else:
-            self._update(key, 'dec')
+            # decrement cnt of the node by 1
+            self.keyMap[key].cnt -= 1
+            # remove from prevCnt list
+            self.cntMap[cnt].remove(key)
+            # add to newCnt list
+            self.cntMap[cnt-1].append(key)
+        
+        if not self.cntMap[cnt]:
+            del self.cntMap[cnt]
+        
+        # print("function: dec", self.cntMap)
 
     def getMaxKey(self) -> str:
-        if not self.key_count:
+        if not self.keyMap:
             return ""
-        return self.tail.pre.get_key()
-
+        
+        maxCnt = max(self.cntMap)
+        # print("function: getMax", self.cntMap)
+        return self.cntMap[maxCnt][-1]
+        
     def getMinKey(self) -> str:
-        if not self.key_count:
+        if not self.keyMap:
             return ""
-        return self.head.next.get_key()
+        minCnt = min(self.cntMap)
+        # print("function: getMin", self.cntMap)
+        
+        return self.cntMap[minCnt][-1]
+
+
+# Your AllOne object will be instantiated and called as such:
+# obj = AllOne()
+# obj.inc(key)
+# obj.dec(key)
+# param_3 = obj.getMaxKey()
+# param_4 = obj.getMinKey()
