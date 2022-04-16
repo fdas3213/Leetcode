3607f48629684388427bd2ea0d8a961451da663c
Shuzhe Xiao <szx9404@gmail.com>
2022-04-16 10:59:23 -0700

    Time: 447 ms (83.64%), Space: 61.4 MB (61.54%) - LeetHub

--- 380-insert-delete-getrandom-o1/380-insert-delete-getrandom-o1.py
+++ 380-insert-delete-getrandom-o1/380-insert-delete-getrandom-o1.py
@@ -1,41 +1,44 @@
 class RandomizedSet:
 
     def __init__(self):
-        # val map to the position it gets added to the hashmap
-        self.valMap = defaultdict(int)
-        # position map to val
-        self.posMap = defaultdict(int)
-        self.count = 0
+        #maps the index of the number to the number. 
+        #index is the sequence in which the number is added to the RandomizedSet
+        self.indexNumMap = defaultdict(int)
+        self.numIndexMap = defaultdict(int)
+        self.index = 0
         
     def insert(self, val: int) -> bool:
-        if val in self.valMap:
+        if val in self.numIndexMap:
             return False
-        self.valMap[val] = self.count
-        self.posMap[self.count] = val
-        self.count += 1
+        self.indexNumMap[self.index] = val
+        self.numIndexMap[val] = self.index
+        self.index += 1
+        
         return True
 
     def remove(self, val: int) -> bool:
-        if val not in self.valMap:
+        if val not in self.numIndexMap:
             return False
-        pos = self.valMap[val]
-        del self.valMap[val]
-        del self.posMap[pos]
         
-        self.count -= 1
-        if not self.posMap:
-            return True
+        curIdx = self.numIndexMap[val]
+        lastVal = self.indexNumMap[self.index-1]
+        # put the last element to the index of the removed element
+        self.indexNumMap[curIdx] = self.indexNumMap[self.index-1]
+        # also update index-to-num mapping
+        self.numIndexMap[lastVal] = curIdx
         
-        last_element = self.posMap[self.count]
-        self.posMap[pos] = last_element
-        self.valMap[last_element] = pos
-        
+        #remove val from num-to-index mapping 
+        del self.numIndexMap[val]
+        del self.indexNumMap[self.index-1]
+    
+        # decrement count by 1
+        self.index -= 1
         
         return True
-
+        
     def getRandom(self) -> int:
-        pos = random.randint(0, self.count-1)
-        return self.posMap[pos]
+        r = random.randint(0, self.index-1)
+        return self.indexNumMap[r]
 
 
 # Your RandomizedSet object will be instantiated and called as such:
