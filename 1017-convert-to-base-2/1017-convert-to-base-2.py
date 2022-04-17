ab42afb856c778cc506ebc6a2aa5c29d82118d05
Shuzhe Xiao <szx9404@gmail.com>
2022-04-17 15:19:31 -0700

    Time: 35 ms (72.92%), Space: 14 MB (11.55%) - LeetHub

--- 1017-convert-to-base-2/1017-convert-to-base-2.py
+++ 1017-convert-to-base-2/1017-convert-to-base-2.py
@@ -2,7 +2,14 @@ class Solution:
     def baseNeg2(self, n: int) -> str:
         if n in [0,1]:
             return str(n)
-        if n%2 == 0:
-            return self.baseNeg2(n//-2) +'0'
-        else:
-            return self.baseNeg2((n-1)//-2) + '1'
+        
+        def dfs(n, cur):
+            if n==0:
+                return cur
+            
+            if n%-2==0:
+                return dfs(n//-2, "0"+cur)
+            else:
+                return dfs((n-1)//-2, "1"+cur)
+        
+        return dfs(n, "")
