28ae6c92d19fc10cc58b1002b123a747264d7459
Shuzhe Xiao <szx9404@gmail.com>
2022-04-17 12:31:44 -0700

    Time: 70 ms (83.89%), Space: 14.7 MB (76.41%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.py
@@ -1,40 +1,40 @@
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
         
-        def partition(left, right):
+        def partition(left, right, nums):
             i, j = left, right
-            # randomly choose a pivot for optimization
-            r = random.randint(left, right)
+            #choose a random pivor
+            r = random.randint(i, j)
             pivot = nums[r]
-            # swap pivot with the first element
+            #swap the random number with the first element
             nums[i], nums[r] = nums[r], nums[i]
             
             while i<j:
-                #place all elements less than pivot to the left of pivot
-                while i<right and nums[i]<=pivot:
+                while i<len(nums) and nums[i]<=pivot:
                     i += 1
-                #place all elements greater than pivot to the right of pivot
                 while j>0 and nums[j]>pivot:
                     j -= 1
                 
+                #swap
                 if i<j:
                     nums[i], nums[j] = nums[j], nums[i]
             
-            #swap with pivot
-            nums[j], nums[left] = nums[left], nums[j]
+            #swap j with the pivot number
+            temp = nums[j]
+            nums[j] = pivot
+            nums[left] = temp
+            
             return j
-        
+    
         left, right = 0, len(nums)-1
-        #kth largest element is the same as N - kth smallest element. 
-        #[1,2,3,4,5], 2nd largest element is the 3rd smallest element
         k = len(nums)-k
         while left<right:
-            pivotIndex = partition(left, right)
-            if pivotIndex==k:
+            pivotIdx = partition(left, right, nums)
+            if pivotIdx==k:
                 return nums[k]
-            elif pivotIndex>k:
-                right = pivotIndex-1
+            elif pivotIdx<k:
+                left = pivotIdx+1
             else:
-                left = pivotIndex+1
+                right = pivotIdx -1
         
         return nums[k]
