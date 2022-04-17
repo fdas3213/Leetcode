10b889dc53a6f176281df893b14ff5cca5baf845
Shuzhe Xiao <szx9404@gmail.com>
2022-04-17 11:46:03 -0700

    Time: 92 ms (96.69%), Space: 15 MB (48.38%) - LeetHub

--- 254-factor-combinations/254-factor-combinations.py
+++ 254-factor-combinations/254-factor-combinations.py
@@ -1,19 +1,18 @@
 class Solution:
     def getFactors(self, n: int) -> List[List[int]]:
         res = []
-
-        def dfs(temp: List[int], i: int):
-            num = temp.pop()
+        
+        def dfs(temp, i):
+            # pop the last element
+            n = temp.pop()
             
-            #to avoid duplicate
-            while i*i<=num:
-                div = num/i
-                if num%i==0:
+            while i*i<=n:
+                if n%i==0:
+                    div = n//i
                     res.append(temp+[i, div])
                     dfs(temp+[i,div], i)
-                i+=1
+                i += 1
         
         dfs([n], 2)
         
         return res
-            
