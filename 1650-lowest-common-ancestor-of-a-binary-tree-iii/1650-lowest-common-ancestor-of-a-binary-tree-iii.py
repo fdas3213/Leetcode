32dc9c732a1c3027ddebd1ef75e487d06b9b899f
Shuzhe Xiao <szx9404@gmail.com>
2022-04-17 15:34:48 -0700

    Time: 71 ms (88.95%), Space: 18.5 MB (34.52%) - LeetHub

--- 1650-lowest-common-ancestor-of-a-binary-tree-iii/1650-lowest-common-ancestor-of-a-binary-tree-iii.py
+++ 1650-lowest-common-ancestor-of-a-binary-tree-iii/1650-lowest-common-ancestor-of-a-binary-tree-iii.py
@@ -12,11 +12,6 @@ class Solution:
     def lowestCommonAncestor(self, p: 'Node', q: 'Node') -> 'Node':
         parent_set = set()
         
-        """
-        Idea: swim up p node to add all possible parents to the set.
-        Then swim up q node, and when q node is found in the set, return the node.
-        """
-        
         while p:
             parent_set.add(p)
             p = p.parent
@@ -26,4 +21,4 @@ class Solution:
                 return q
             q = q.parent
         
-        return p
+        return None
