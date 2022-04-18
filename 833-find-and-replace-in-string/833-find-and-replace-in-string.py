7c7de3b173fd916925c81c9482d46ecc99cd9b1e
Shuzhe Xiao <szx9404@gmail.com>
2022-04-17 18:36:30 -0700

    Time: 34 ms (95.16%), Space: 13.8 MB (94.96%) - LeetHub

--- 833-find-and-replace-in-string/833-find-and-replace-in-string.py
+++ 833-find-and-replace-in-string/833-find-and-replace-in-string.py
@@ -1,21 +1,23 @@
 class Solution:
     def findReplaceString(self, s: str, indices: List[int], sources: List[str], targets: List[str]) -> str:
-        ans = ""
+        srcTgtMap = {}
+        indexSrcMap = {}
         
-        strMap = defaultdict(int)
-        
-        # store the indices whose corresponding is found at s[index]. For example, index=0, source=a, s='abcd', then because source is found at s[0], we add index to the hashmap. The value of the hashmap is just the index of sources/targets, which is later used to replace the substring and retrieve substring length.
-        for i, (index, source) in enumerate(zip(indices, sources)):
+        for i, (index, source, target) in enumerate(zip(indices, sources, targets)):
             if s[index:].startswith(source):
-                strMap[index] = i
+                srcTgtMap[index] = target
+                indexSrcMap[index] = i
         
+        ans = ""
         start = 0
-        while start<len(s):
-            if start in strMap:
-                ans += targets[strMap[start]]
-                start += len(sources[strMap[start]])
-            else:
+        print(srcTgtMap)
+        while start < len(s):
+            if start not in srcTgtMap:
                 ans += s[start]
                 start += 1
+            else:
+                ans += srcTgtMap[start]
+                prevLen = len(sources[indexSrcMap[start]])
+                start += prevLen
         
         return ans
