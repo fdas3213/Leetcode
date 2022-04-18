16acb4867a094b9470f7169c31ff35e9bd5ed05e
Shuzhe Xiao <szx9404@gmail.com>
2022-04-17 18:26:00 -0700

    Time: 83 ms (96.18%), Space: 15.5 MB (11.63%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,26 +1,31 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        if not nums or target>nums[-1] or target<nums[0]:
-            return [-1,-1]
         
-        def bisect_left(nums, target):
-            start, end = 0, len(nums)-1
-            while start < end:
-                mid = start + (end-start)//2
-                if nums[mid] < target:
-                    start = mid +1
+        if not nums or target<nums[0] or target>nums[-1]:
+            return [-1, -1]
+        
+        def findFirst(nums, target):
+            left, right = 0, len(nums)-1
+            while left<right:
+                mid = left+(right-left)//2
+                if nums[mid]<target:
+                    left = mid+1
                 else:
-                    end = mid
-            return start if nums[start]==target else -1
+                    right = mid
+            
+            return left if nums[left]==target else -1
         
-        def bisect_right(nums, target):
-            start, end = 0, len(nums)-1
-            while start < end:
-                mid = start + (end-start)//2+1
-                if nums[mid] > target:
-                    end = mid -1
+        def findLast(nums, target):
+            left, right = 0, len(nums)-1
+            while left<right:
+                mid = left+(right-left)//2+1
+                if nums[mid]>target:
+                    right = mid-1
                 else:
-                    start = mid
-            return start if nums[start]==target else -1
+                    left = mid
+            
+            return right if nums[right]==target else -1
         
-        return [bisect_left(nums,target), bisect_right(nums, target)]
+        return [findFirst(nums, target), findLast(nums, target)]
+                
+                
