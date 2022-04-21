cc10b6bd2e178db0737916e8b38fa720ec32dba0
Shuzhe Xiao <szx9404@gmail.com>
2022-04-20 22:04:27 -0700

    Time: 53 ms (60.94%), Space: 13.9 MB (54.55%) - LeetHub

--- 360-sort-transformed-array/360-sort-transformed-array.py
+++ 360-sort-transformed-array/360-sort-transformed-array.py
@@ -1,38 +1,32 @@
 class Solution:
     def sortTransformedArray(self, nums: List[int], a: int, b: int, c: int) -> List[int]:
         
-        def compute(x):
+        def calc(x, a, b, c):
             return a*x*x+b*x+c
         
         l = len(nums)
-        #initialize result
-        res = [0 for _ in range(l)]
-        
-        p = 0 if a<0 else l-1
+        ans = [0 for _ in range(l)]
+        pointer = l-1 if a>=0 else 0
         left, right = 0, l-1
         
         while left<=right:
-            left_n, right_n = nums[left], nums[right]
-            left_v, right_v = compute(left_n), compute(right_n)
+            left_v = calc(nums[left], a, b, c)
+            right_v = calc(nums[right], a, b, c)
             if a>=0:
-                #in this case, we fill out the result array from the tail
                 if left_v>=right_v:
-                    res[p] = left_v
+                    ans[pointer]=left_v
                     left += 1
                 else:
-                    res[p] = right_v
+                    ans[pointer]=right_v
                     right -= 1
-                #decrement p since we're filling in bigger values first
-                p -= 1
+                pointer -= 1
             else:
-                #in this case, we fill out the result array from the head
                 if left_v<=right_v:
-                    res[p] = left_v
+                    ans[pointer]=left_v
                     left += 1
                 else:
-                    res[p] = right_v
+                    ans[pointer]=right_v
                     right -= 1
-                #increment p since we're filling in smaller values first
-                p += 1
-        
-        return res
+                pointer += 1
+            
+        return ans
