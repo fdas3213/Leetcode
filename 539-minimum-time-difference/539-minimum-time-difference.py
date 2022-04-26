45cf5267ed825e61a46f91918319e0c730638a6c
Shuzhe Xiao <szx9404@gmail.com>
2022-04-26 14:53:00 -0700

    Time: 118 ms (31.18%), Space: 17.3 MB (37.59%) - LeetHub

--- 539-minimum-time-difference/539-minimum-time-difference.py
+++ 539-minimum-time-difference/539-minimum-time-difference.py
@@ -1,20 +1,18 @@
 class Solution:
     def findMinDifference(self, timePoints: List[str]) -> int:
-        minutesOfDay = 1440
+        minutesOfDay =1440
         
-        normalizedTime = []
-        #convert time to absolute minute of the day
-        for time in timePoints:
+        def getTime(time):
             hr, minute = time.split(":")
             total = 0
             if hr!='00':
                 total += int(hr)*60
-            if minute!='00':
+            if minute !='00':
                 total += int(minute)
-            
-            normalizedTime.append(total)
-        #sort the time so that the minimum value must be two adjacent values
+            return total
+        
+        normalizedTime = [getTime(time) for time in timePoints]
         normalizedTime.sort()
         
-        return min((y-x)%minutesOfDay for x,y in zip(normalizedTime,
-                                                    normalizedTime[1:]+normalizedTime[:1]))
+        
+        return min((y-x)%minutesOfDay for x,y in zip(normalizedTime, normalizedTime[1:]+normalizedTime[:1]))
