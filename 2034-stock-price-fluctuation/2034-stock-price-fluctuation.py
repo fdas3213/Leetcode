43bbac980f51646e74511484fe7a2ac0c9473c7d
Shuzhe Xiao <szx9404@gmail.com>
2022-04-26 17:00:42 -0700

    Time: 893 ms (77.65%), Space: 59.7 MB (77.53%) - LeetHub

--- 2034-stock-price-fluctuation/2034-stock-price-fluctuation.py
+++ 2034-stock-price-fluctuation/2034-stock-price-fluctuation.py
@@ -1,43 +1,36 @@
 class StockPrice:
 
     def __init__(self):
-        #hashmap to store timestamp and price
-        self.stockPrice = defaultdict(int)
-        # use two heaps to update max and min price
+        self.timePrice = defaultdict(int)
+        self.time = 0
         self.maxPrice = []
         self.minPrice = []
-        # used to track the current timestamp
-        self.maxTimeStamp = 0
-        
-    def update(self, timestamp: int, price: int) -> None:                   
-        self.stockPrice[timestamp] = price
-        self.maxTimeStamp = max(self.maxTimeStamp, timestamp)
+
+    def update(self, timestamp: int, price: int) -> None:
+        self.timePrice[timestamp] = price
+        self.time = max(self.time, timestamp)
         
         heappush(self.maxPrice, (-price, timestamp))
         heappush(self.minPrice, (price, timestamp))
         
     def current(self) -> int:
-        return self.stockPrice[self.maxTimeStamp]
+        return self.timePrice[self.time]
 
     def maximum(self) -> int:
-        curPrice, timestamp = heappop(self.maxPrice)
-        while -curPrice != self.stockPrice[timestamp]:
-            # this means the price of the current timestamp has been updated, and we can discard the old price
-            curPrice, timestamp = heappop(self.maxPrice)
-        
-        # add the correct one back to the heap
-        heappush(self.maxPrice, (curPrice, timestamp))
+        price, time = heappop(self.maxPrice)
+        while -price != self.timePrice[time]:
+            price, time = heappop(self.maxPrice)
+        #need to push the last element back into the heap
+        heappush(self.maxPrice,(price, time))
         
         return -self.maxPrice[0][0]
 
     def minimum(self) -> int:
-        curPrice, timestamp = heappop(self.minPrice)
-        while curPrice != self.stockPrice[timestamp]:
-            # this means the price of the current timestamp has been updated, and we can discard the old price
-            curPrice, timestamp = heappop(self.minPrice)
-        
-        # add the correct one back to the heap
-        heappush(self.minPrice, (curPrice, timestamp))
+        price, time = heappop(self.minPrice)
+        while price != self.timePrice[time]:
+            price, time = heappop(self.minPrice)
+        #need to push the last element back into the heap
+        heappush(self.minPrice,(price, time))
         
         return self.minPrice[0][0]
 
