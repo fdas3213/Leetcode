59bd16bcc30545d4748e218f5c8873ba344a260c
Shuzhe Xiao <szx9404@gmail.com>
2022-04-26 17:42:10 -0700

    Time: 366 ms (73.79%), Space: 16 MB (25.68%) - LeetHub

--- 2013-detect-squares/2013-detect-squares.py
+++ 2013-detect-squares/2013-detect-squares.py
@@ -1,23 +1,25 @@
 class DetectSquares:
 
     def __init__(self):
+        #use a hashmap to store {points: count}
         self.points = Counter()
 
     def add(self, point: List[int]) -> None:
-        x,y=point
+        x, y = point
         self.points[(x,y)] += 1
 
     def count(self, point: List[int]) -> int:
-        x1, y1 = point
+        #check if there exists a point (x1,y1) s.t. x3-x1==y1-y3
+        x3, y3 = point
         ans = 0
-        for (x3,y3), cnt in self.points.items():
-            # check if there exists a diagonal point
-            if abs(x1-x3)!=abs(y1-y3) or x1-x3==0:
-                continue
-            # need to check if there exist two points(p1.x, p2.y) and (p2.x,p1.y)
-            ans += cnt*self.points[(x1,y3)]*self.points[(x3,y1)]
-                
+        for (x1,y1), cnt in self.points.items():
+            if x1!=x3 and abs(x1-x3)==abs(y1-y3):
+                p2_cnt = self.points[(x1, y3)]
+                p4_cnt = self.points[(x3, y1)]
+                ans += cnt*p2_cnt*p4_cnt
+        
         return ans
+            
 
 
 # Your DetectSquares object will be instantiated and called as such:
