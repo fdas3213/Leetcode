283d76474e88c8585bd4525ad41a10b0f6f32e3f
Shuzhe Xiao <szx9404@gmail.com>
2022-04-26 23:02:35 -0700

    Time: 1906 ms (49.98%), Space: 32.8 MB (56.98%) - LeetHub

--- 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.py
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.py
@@ -1,29 +1,25 @@
 class Solution:
     def findOriginalArray(self, changed: List[int]) -> List[int]:
+        #sort the input array
+        changed.sort()
         res = []
         
-        counts = Counter(changed)
-        changed.sort()
-        l = len(changed)
+        numCnt = Counter(changed)
         
-        for i, n in enumerate(changed):
-            # skip numbers whose count is 0 because they've already been a double of other numbers
-            if counts[n]==0:
+        for n in changed:
+            #check freq of current number
+            if numCnt[n]==0:
                 continue
-            doubled = n*2
-            if n==0 and counts[n]%2 !=0:
-                return []
-                # #because 0 is a double of 0, we decrement 2
-                # counts[n]-=2
-                # res.append(n)
-                # continue
-            
-            if doubled not in counts or counts[doubled]==0:
+            #if current number is 0 and frequency is not even
+            if n==0 and numCnt[n]%2!=0:
+                return res
+            #need to decrement both n and n*2
+            double = n*2
+            if double not in numCnt or numCnt[double]<=0:
                 return []
             
-            # decrement the count of both the current number and its double 
-            counts[n]-=1
-            counts[doubled]-=1
+            numCnt[n] -= 1
+            numCnt[double] -= 1
             res.append(n)
         
         return res
