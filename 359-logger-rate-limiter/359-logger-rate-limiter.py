eed018d7bf87ffc2ad5124ee14fe4ecf479b3d48
Shuzhe Xiao <szx9404@gmail.com>
2022-04-26 22:52:37 -0700

    Time: 194 ms (47.01%), Space: 20.8 MB (36.61%) - LeetHub

--- 359-logger-rate-limiter/359-logger-rate-limiter.py
+++ 359-logger-rate-limiter/359-logger-rate-limiter.py
@@ -1,17 +1,19 @@
 class Logger:
 
     def __init__(self):
-        #use a hashmap to store the message and its previous timestamp
-        self.messageMap = {}
+        self.messageMap = defaultdict(int)
 
     def shouldPrintMessage(self, timestamp: int, message: str) -> bool:
-        prevTime = self.messageMap.get(message, -1)
-        
-        if prevTime==-1 or timestamp-prevTime>=10:
+        if message not in self.messageMap:
             self.messageMap[message] = timestamp
             return True
         else:
-            return False
+            if timestamp-self.messageMap[message]<10:
+                return False
+            #update the timestamp for the incoming message
+            self.messageMap[message]=timestamp
+            return True
+    
 
 
 # Your Logger object will be instantiated and called as such:
